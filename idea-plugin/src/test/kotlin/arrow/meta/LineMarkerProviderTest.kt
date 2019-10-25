7cf6a6f22913853dd889f79f3da4de1e498bdeb7
jansorg <github@joachim-ansorg.de>
2019-10-25 14:30:32 +0200

    Fix rebase with master

--- idea-plugin/src/test/kotlin/arrow/meta/LineMarkerProviderTest.kt
+++ idea-plugin/src/test/kotlin/arrow/meta/LineMarkerProviderTest.kt
@@ -10,64 +10,64 @@ import org.intellij.lang.annotations.Language
 import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.junit.Test
 
-class LineMarkerProviderTest : KotlinIdeBaseTest() {
+class LineMarkerProviderTest : IdeBaseTestSyntax() {
   @Language("kotlin")
   private val withMarkers = """
-        package test
-        import arrow.higherKind
-        
-        @higherkind
-        class <caret>Id1<out A>(val value: A)
-        
-        @arrow.higherkind
-        class <caret>Id2<out A>(val value: A)
-         
-        val x: IdOf<Int> = Id(1)
-        """.trimIndent()
+    package test
+    import arrow.higherKind
+    
+    @higherkind
+    class <caret>Id1<out A>(val value: A)
+    
+    @arrow.higherkind
+    class <caret>Id2<out A>(val value: A)
+     
+    val x: IdOf<Int> = Id(1)
+    """.trimIndent()
 
   @Language("kotlin")
   private val withoutMarkers = """
-        package test
-        import arrow.higherKind
-
-        sealed class Sealed
-        @higherKind
-        data class <caret>IdSealed<out A>(val number: A) : Sealed()
-        
-        // missing @higherKind
-        class <caret>IdNoHigherKind<out A>(val value: A)
-        
-        // annotation class
-        @higherKind
-        annotation class <caret>IdAnnotation<out A>(val value: A)
-        
-        // missing type parameter
-        @higherKind
-        annotation class <caret>IdNoTypeParameter(val value: Int)
-        
-        // missing marker, no type parameter
-        class <caret>Outer {
-          // nested class
-          @higherkind
-          class <caret>Nested<out A>(val value: A)
-        }
-        
-        fun foo() {
-          // not at top-level
-          @higherkind
-          class <caret>NotTopLevel<out A>(val value: A) 
-        }
-
-        // not a class
-        val <caret>notAClass: IdOf<Int> = Id(1)
-        """.trimIndent()
+    package test
+    import arrow.higherKind
+  
+    sealed class Sealed
+    @higherKind
+    data class <caret>IdSealed<out A>(val number: A) : Sealed()
+    
+    // missing @higherKind
+    class <caret>IdNoHigherKind<out A>(val value: A)
+    
+    // annotation class
+    @higherKind
+    annotation class <caret>IdAnnotation<out A>(val value: A)
+    
+    // missing type parameter
+    @higherKind
+    annotation class <caret>IdNoTypeParameter(val value: Int)
+    
+    // missing marker, no type parameter
+    class <caret>Outer {
+      // nested class
+      @higherkind
+      class <caret>Nested<out A>(val value: A)
+    }
+    
+    fun foo() {
+      // not at top-level
+      @higherkind
+      class <caret>NotTopLevel<out A>(val value: A) 
+    }
+  
+    // not a class
+    val <caret>notAClass: IdOf<Int> = Id(1)
+    """.trimIndent()
 
   @Test
   fun testAvailable() {
     // if possible, line marker providers should provide icons for leaf elements
     // as advised by com.intellij.codeInsight.daemon.LineMarkerProvider.getLineMarkerInfo().
     // Therefore, we retrieve the leaf element for the current element and call the icon providers on this leaf
-    withEachCaret(withMarkers) { psi ->
+    withMarkers.withEachCaret { psi ->
       val leaf = if (psi.firstChild == null) psi else PsiTreeUtil.getDeepestFirst(psi)
 
       assertEquals("expected one fast marker to be provided by arrow, element: ${leaf.text}", 1, leaf.fastArrowMarkers().size)
@@ -86,7 +86,7 @@ class LineMarkerProviderTest : KotlinIdeBaseTest() {
 
   @Test
   fun testUnavailable() {
-    withEachCaret(withoutMarkers) { psi ->
+    withoutMarkers.withEachCaret { psi ->
       assertEmpty("no fast markers exepcted for ${psi.text}", psi.fastArrowMarkers())
 
       assertEmpty("no slow markers exepcted for ${psi.text}", psi.slowArrowMarkers())
