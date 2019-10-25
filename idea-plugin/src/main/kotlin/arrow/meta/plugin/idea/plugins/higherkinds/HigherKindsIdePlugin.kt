c24b21d9a71aa83a53499bbe927b2974e4331c70
jansorg <github@joachim-ansorg.de>
2019-10-25 12:36:40 +0200

    Add a test case for line markers. Change provider to implement the fast, recommended way to provide line markers.

--- idea-plugin/src/main/kotlin/arrow/meta/plugin/idea/plugins/higherkinds/HigherKindsIdePlugin.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/plugin/idea/plugins/higherkinds/HigherKindsIdePlugin.kt
@@ -5,16 +5,20 @@ import arrow.meta.invoke
 import arrow.meta.plugin.idea.IdeMetaPlugin
 import arrow.meta.plugin.idea.resources.ArrowIcons
 import arrow.meta.plugins.higherkind.isHigherKindedType
+import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 val IdeMetaPlugin.higherKindsIdePlugin: Plugin
   get() = "HigherKindsIdePlugin" {
     meta(
-      addLineMarkerProvider(
+      addFastLineMarkerProvider(
         icon = ArrowIcons.HKT,
         message = "HKT",
-        matchOn = { it.safeAs<KtClass>()?.let(::isHigherKindedType) == true }
+        matchOn = {
+          KtTokens.IDENTIFIER == it.node?.elementType
+            && it.parent.safeAs<KtClass>()?.let(::isHigherKindedType) == true
+        }
       )
     )
   }
