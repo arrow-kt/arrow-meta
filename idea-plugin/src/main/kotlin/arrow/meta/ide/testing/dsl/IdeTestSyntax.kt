081ac680fb657741d4490527e1023d4c157a9b43
i-walker <46971368+i-walker@users.noreply.github.com>
2019-11-01 14:19:06 +0100

    iconProvider support

--- idea-plugin/src/main/kotlin/arrow/meta/ide/testing/dsl/IdeTestSyntax.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/testing/dsl/IdeTestSyntax.kt
@@ -1,6 +1,7 @@
 package arrow.meta.ide.testing.dsl
 
+import arrow.meta.ide.testing.dsl.icon.IconProviderTestSyntax
 import arrow.meta.ide.testing.dsl.lineMarker.LineMarkerTestSyntax
 import arrow.meta.ide.testing.env.IdeTestTypeSyntax
 
-interface IdeTestSyntax : IdeTestTypeSyntax, LineMarkerTestSyntax
+interface IdeTestSyntax : IdeTestTypeSyntax, LineMarkerTestSyntax, IconProviderTestSyntax
