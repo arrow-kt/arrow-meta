9ab24a39c4ddd410e5bef95ed579c51ae10046b6
Raul Raja <raulraja@gmail.com>
2019-11-03 23:34:44 +0100

    First Draft at Union Types

--- compiler-plugin/src/main/kotlin/arrow/meta/plugins/union/UnionsPlugin.kt
+++ compiler-plugin/src/main/kotlin/arrow/meta/plugins/union/UnionsPlugin.kt
@@ -3,7 +3,20 @@ package arrow.meta.plugins.union
 import arrow.meta.Meta
 import arrow.meta.Plugin
 import arrow.meta.invoke
-import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
+import arrow.meta.phases.codegen.ir.IrUtils
+import org.jetbrains.kotlin.descriptors.findClassAcrossModuleDependencies
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
+import org.jetbrains.kotlin.ir.declarations.IrVariable
+import org.jetbrains.kotlin.ir.expressions.IrExpression
+import org.jetbrains.kotlin.ir.expressions.IrReturn
+import org.jetbrains.kotlin.ir.expressions.impl.IrReturnImpl
+import org.jetbrains.kotlin.ir.types.impl.originalKotlinType
+import org.jetbrains.kotlin.ir.types.toKotlinType
+import org.jetbrains.kotlin.ir.util.ConstantValueGenerator
+import org.jetbrains.kotlin.ir.util.TypeTranslator
+import org.jetbrains.kotlin.ir.util.dump
+import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.types.KotlinType
 import org.jetbrains.kotlin.types.checker.KotlinTypeChecker
 import org.jetbrains.kotlin.types.typeUtil.asTypeProjection
@@ -13,32 +26,97 @@ val Meta.unionTypes: Plugin
   get() =
     "Union Types" {
       meta(
-        typeChecker(::UnionTypeChecker)
+        typeChecker(::UnionTypeChecker),
+        suppressDiagnostic {
+          if (it.factory == Errors.TYPE_MISMATCH) {
+            val typeMismatch = Errors.TYPE_MISMATCH.cast(it)
+            val result = typeMismatch.b.nullableUnionTargets(subType = typeMismatch.a)
+            println("type mismatch suppress: ${typeMismatch.a} ${typeMismatch.b} $result")
+            result
+          } else false
+        },
+        irVariable {
+          println("irVariable:\n${it.dump()}")
+          val targetType = it.type.originalKotlinType
+          val valueType = it.initializer?.type?.originalKotlinType
+          if (targetType != null && valueType != null && targetType.union(valueType)) { //insert conversion
+            applyUnionLift(it)
+          } else it
+        },
+        irReturn {
+          val targetType = it.returnTarget.returnType
+          val valueType = it.value.type.originalKotlinType
+          if (targetType != null && valueType != null && targetType.union(valueType)) { //insert conversion
+            applyUnionLift(it, targetType)
+          } else it
+        },
+        IrGeneration { compilerContext, file, backendContext, bindingContext ->
+          println(file.dump())
+        }
       )
     }
 
-
-class UnionTypeChecker(val typeChecker: KotlinTypeChecker) : KotlinTypeChecker by typeChecker {
-
-  private fun KotlinType.isUnion(): Boolean {
-    println("type: " + constructor.declarationDescriptor?.fqNameSafe?.asString())
-    return constructor.declarationDescriptor?.fqNameSafe?.asString()?.startsWith("Union") == true
+fun IrUtils.applyUnionLift(intercepted: IrVariable): IrVariable? =
+  intercepted.apply {
+    initializer = unionClassDescriptor()?.irConstructorCall()?.apply {
+      putValueArgument(0, initializer)
+    }
   }
 
-  private fun KotlinType.union(other: KotlinType): Boolean =
-    isUnion() && arguments.contains(other.asTypeProjection())
+private fun IrUtils.unionClassDescriptor() =
+  backendContext.ir.irModule.descriptor.findClassAcrossModuleDependencies(ClassId.fromString("Union"))
 
-  private fun KotlinType.nullableUnionTargets(other: KotlinType): Boolean =
-   other.isMarkedNullable && isUnion() && arguments.contains(other.makeNotNullable().asTypeProjection())
+fun IrUtils.applyUnionLift(intercepted: IrReturn, targetType: KotlinType?): IrExpression? {
+  return targetType?.let { unionType ->
+    val unionImpl = backendContext.ir.irModule.descriptor.findClassAcrossModuleDependencies(ClassId.fromString("Union"))
+    return unionImpl?.irConstructorCall()?.let { constructorCall ->
+      constructorCall.putValueArgument(0, intercepted.value)
+      IrReturnImpl(
+        UNDEFINED_OFFSET,
+        UNDEFINED_OFFSET,
+        typeTranslator().translateType(unionType),
+        intercepted.returnTargetSymbol,
+        constructorCall
+      )
+    }
+  }
+}
+
+private fun IrUtils.typeTranslator(): TypeTranslator =
+  TypeTranslator(
+    symbolTable = backendContext.ir.symbols.externalSymbolTable,
+    languageVersionSettings = backendContext.irBuiltIns.languageVersionSettings,
+    builtIns = backendContext.builtIns
+  ).apply {
+    constantValueGenerator =
+      ConstantValueGenerator(
+        moduleDescriptor = backendContext.ir.irModule.descriptor,
+        symbolTable = backendContext.ir.symbols.externalSymbolTable
+      )
+  }
+
+
+private fun KotlinType.nullableUnionTargets(subType: KotlinType): Boolean =
+  subType.isMarkedNullable && isUnion() && arguments.contains(subType.makeNotNullable().asTypeProjection())
+
+private fun KotlinType.isUnion(): Boolean {
+  println("type: " + constructor.declarationDescriptor?.name?.asString())
+  return constructor.declarationDescriptor?.name?.asString()?.startsWith("Union") == true
+}
+
+private fun KotlinType.union(subType: KotlinType) =
+  isUnion() && arguments.contains(subType.asTypeProjection()) || nullableUnionTargets(subType)
+
+class UnionTypeChecker(val typeChecker: KotlinTypeChecker) : KotlinTypeChecker by typeChecker {
 
   override fun isSubtypeOf(p0: KotlinType, p1: KotlinType): Boolean {
     val underlyingResult = typeChecker.isSubtypeOf(p0, p1)
     return if (!underlyingResult) {
       val subType = p0.unwrap()
       val superType = p1.unwrap()
-      val inUnion: Boolean = superType.union(subType) || subType.nullableUnionTargets(superType)
+      val inUnion: Boolean = superType.union(subType)
       val result = inUnion
-      println("UnionTypeChecker.isSubtypeOf: $p0 <-> $p1 = $result, inUnion: $inUnion")
+      println("UnionTypeChecker.isSubtypeOf: $subType <-> $superType = $result, inUnion: $inUnion")
       result
     } else underlyingResult
   }
@@ -51,5 +129,6 @@ class UnionTypeChecker(val typeChecker: KotlinTypeChecker) : KotlinTypeChecker b
   }
 
 }
+
 
 
--- compiler-plugin/src/test/kotlin/arrow/meta/plugins/union/UnionTest.kt
+++ compiler-plugin/src/test/kotlin/arrow/meta/plugins/union/UnionTest.kt
@@ -1,6 +1,5 @@
 package arrow.meta.plugins.union
 
-import arrow.core.Some
 import arrow.meta.plugin.testing.CompilerPlugin
 import arrow.meta.plugin.testing.CompilerTest
 import arrow.meta.plugin.testing.Dependency
@@ -20,20 +19,40 @@ class UnionTest {
       },
       code = {
         """
-        | interface Union2<out A, out B>
-        | 
-        | typealias Result = Union2<String, Int>
-        | 
-        | fun f1(): Result = "a"
-        | fun f2(): Result = 1
-        | fun f3(): String? = f1()
-        | fun f4(): Int? = f2()
+        |${UnionTestPrelude}
+        |
+        |fun x1(): Union2<String, Int> = Union("a")
+        |fun f1(): Union2<String, Int> = "a"
+        |fun f12(): Union2<String, Int> { 
+        | val s = "x"
+        | val z: Union2<String, Int> = s
+        | return z
+        |}
+        |fun f2(): Union2<String, Int> = 1
+        |fun f3(): String? = f1()
+        |fun f4(): Int? = f2()
+        |fun f5(): Union2<String, Int> = "a" as Union2<String, Int>
         | 
         """.source
       },
       assert = {
-        "f1()".source.evalsTo("a")
+        "f12().value".source.evalsTo("x")
       }
     ))
   }
+
+  private val UnionTestPrelude: String =
+    """|
+       |interface Union2<out A, out B>
+        |interface Union3<out A, out B, out C> : Union2<A, B>
+        |interface Union4<out A, out B, out C, out D> : Union3<A, B, C>
+        |
+        |inline class Union(val value: Any?) :
+        |  Union2<Nothing, Nothing>,
+        |  Union3<Nothing, Nothing, Nothing>,
+        |  Union4<Nothing, Nothing, Nothing, Nothing> {
+        |
+        |   inline operator fun <reified A> invoke(): A? =
+        |     value as? A
+        |}""".trimMargin()
 }
