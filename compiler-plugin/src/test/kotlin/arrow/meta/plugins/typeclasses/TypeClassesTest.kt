6e0d8e3ccaf5f8d78b4ce9114d3cf5186d53526f
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2019-11-05 20:32:55 +0100

    testing-plugin: multiple asserts (#68)

--- compiler-plugin/src/test/kotlin/arrow/meta/plugins/typeclasses/TypeClassesTest.kt
+++ compiler-plugin/src/test/kotlin/arrow/meta/plugins/typeclasses/TypeClassesTest.kt
@@ -96,18 +96,7 @@ class TypeClassesTest {
           |     return Some(1).addOne()
           |   }
           | }
-          |""".source)
-      }
-    ))
-
-    assertThis(CompilerTest(
-      config = {
-        addCompilerPlugins(compilerPlugin) + addDependencies(arrowAnnotations, arrowCoreData)
-      },
-      code = {
-        codeSnippet.source
-      },
-      assert = {
+          |""".source) +
         "foo()".source.evalsTo(Some(2))
       }
     ))
