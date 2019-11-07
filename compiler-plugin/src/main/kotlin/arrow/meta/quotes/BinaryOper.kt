3c2db249cca69ec6cece4d4c332e29bd897ab39e
Amanda Hinchman-Dominguez <ahinchman1@gmail.com>
2019-11-07 15:17:23 -0600

    Used Scope wrapper instead of crafting the intermediate class

--- compiler-plugin/src/main/kotlin/arrow/meta/quotes/BinaryOper.kt
+++ compiler-plugin/src/main/kotlin/arrow/meta/quotes/BinaryOper.kt
@@ -2,21 +2,15 @@ package arrow.meta.quotes
 
 import arrow.meta.Meta
 import arrow.meta.phases.ExtensionPhase
+import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtBinaryExpression
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtOperationReferenceExpression
 
 /**
- * A binary expression with an operator can occur in the following instances:
- *  - from an expression
- *  - from an element
- *  - from an argument
- *
- *  Since there are so many ways an operator can be detected, it may be beneficial to drill down
- *  using a more generalized visitor as the top Meta operation....
- *
- *  Below is starting in the wrong place so this will need to start in a more broad area
+ * A template destructuring [Scope] for a [KtBinaryExpression]
  */
 fun Meta.binaryOper(
   match: KtBinaryExpression.() -> Boolean,
@@ -24,26 +18,9 @@ fun Meta.binaryOper(
 ): ExtensionPhase =
   quote(match, map) { BinaryExpressionScope(it) } // How can I drill down into the right scope here?
 
-// TODO need to rescope up to the ??? -> binary operator -> expression -> operation reference expression
-
-class CallExpressionScope(
-  override val value: KtCallExpression
-
-): Scope<KtCallExpression>(value)
-
-// TODO this binary expression needs to be drilled down to primaries or objects, so this will have to be filtered
 class BinaryExpressionScope(
   override val value: KtBinaryExpression,
-  val left: ExpressionScope? = value.left?.let { ExpressionScope(it) },
-  val right: ExpressionScope? = value.right?.let { ExpressionScope(it) },
-  val operationReference: OperationReferenceExpressionScope? = value.operationReference.let { OperationReferenceExpressionScope(it) }
+  val left: Scope<KtExpression>? = Scope(value.left),
+  val right: Scope<KtExpression>? = Scope(value.right),
+  val operationReference: Scope<KtOperationReferenceExpression>? = Scope(value.operationReference)
 ): Scope<KtBinaryExpression>(value)
-
-class ExpressionScope(
-  override val value: KtExpression
-  // val type: KtTypeReference
-): Scope<KtExpression>(value)
-
-class OperationReferenceExpressionScope(
-  override val value: KtOperationReferenceExpression
-): Scope<KtOperationReferenceExpression>(value)
