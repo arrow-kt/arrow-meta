cfcf961b09591083eb6f96c4f25f48e6ed540659
jansorg <github@joachim-ansorg.de>
2019-11-08 14:40:50 +0100

    Add test to test snippet compilation

---
+++ idea-plugin/src/test/kotlin/arrow/meta/ide/plugins/CompilationTest.kt
@@ -0,0 +1,19 @@
+package arrow.meta.ide.plugins
+
+import arrow.meta.ide.testing.env.IdeHeavyTestSetUp
+import arrow.meta.ide.testing.env.types.HeavyTestSyntax.compile
+import com.tschuchort.compiletesting.KotlinCompilation
+
+class CompilationTest : IdeHeavyTestSetUp() {
+  fun testCompilation() {
+    val code = """
+      fun main() {
+        println("hello world")
+      }
+    """
+
+    // this prints an exception to stdout
+    val result = compile(code)
+    assertTrue("compilation of a snippet must succeed", result.exitCode == KotlinCompilation.ExitCode.OK)
+  }
+}
