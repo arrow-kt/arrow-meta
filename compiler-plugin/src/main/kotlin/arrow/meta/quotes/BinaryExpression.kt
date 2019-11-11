49dbee0bfd36f56acf78c6884f50ce2357ffcfbd
Amanda Hinchman-Dominguez <ahinchman1@gmail.com>
2019-11-11 08:43:56 -0600

    `binaryOper` Quote and Template Scope (#90)
    * attempt at a quasi-quote system for a binary operator
    
    * Used Scope wrapper instead of crafting the intermediate class
    
    * Changed shorted names to full Kt Element names

---
+++ compiler-plugin/src/main/kotlin/arrow/meta/quotes/BinaryExpression.kt
@@ -0,0 +1,26 @@
+package arrow.meta.quotes
+
+import arrow.meta.Meta
+import arrow.meta.phases.ExtensionPhase
+import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.psi.KtBinaryExpression
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtOperationReferenceExpression
+
+/**
+ * A template destructuring [Scope] for a [KtBinaryExpression]
+ */
+fun Meta.binaryExpression(
+  match: KtBinaryExpression.() -> Boolean,
+  map: BinaryExpressionScope.(KtBinaryExpression) -> Transform<KtBinaryExpression>
+): ExtensionPhase =
+  quote(match, map) { BinaryExpressionScope(it) } // How can I drill down into the right scope here?
+
+class BinaryExpressionScope(
+  override val value: KtBinaryExpression,
+  val left: Scope<KtExpression>? = Scope(value.left),
+  val right: Scope<KtExpression>? = Scope(value.right),
+  val operationReference: Scope<KtOperationReferenceExpression>? = Scope(value.operationReference)
+): Scope<KtBinaryExpression>(value)
