5b595b5d92441f276f14a48c9960030c20990983
i-walker <46971368+i-walker@users.noreply.github.com>
2019-11-12 14:42:57 +0100

    add Annotation

---
+++ compiler-plugin/src/main/kotlin/arrow/meta/internal/annotation/MetaAnnotations.kt
@@ -0,0 +1,7 @@
+package arrow
+
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.EXPRESSION, AnnotationTarget.CLASS, AnnotationTarget.TYPEALIAS,
+  AnnotationTarget.FIELD, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.LOCAL_VARIABLE,
+  AnnotationTarget.FUNCTION)
+internal annotation class synthetic
