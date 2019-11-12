f6caf0615f9e58ebae8e4d1444dc22ac572459e6
i-walker <46971368+i-walker@users.noreply.github.com>
2019-11-12 20:48:30 +0100

    m

--- compiler-plugin/src/main/kotlin/arrow/meta/internal/annotation/MetaAnnotations.kt
+++ compiler-plugin/src/main/kotlin/arrow/meta/internal/annotation/MetaAnnotations.kt
@@ -3,5 +3,5 @@ package arrow
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.EXPRESSION, AnnotationTarget.CLASS, AnnotationTarget.TYPEALIAS,
   AnnotationTarget.FIELD, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.LOCAL_VARIABLE,
-  AnnotationTarget.FUNCTION)
+  AnnotationTarget.FUNCTION, AnnotationTarget.TYPE_PARAMETER, AnnotationTarget.TYPE)
 internal annotation class synthetic
