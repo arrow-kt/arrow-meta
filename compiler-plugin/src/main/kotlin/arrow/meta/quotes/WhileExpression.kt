b4058475fb8dba83ddd542335ec268af6e8382ea
Bloder <aptgetinstalldanielleme@gmail.com>
2019-11-13 14:08:39 -0300

    While expression quote and scope (#121)
    * Create while expression quote and its scope mapping
    
    * Implement missing element scope while expression props

---
+++ compiler-plugin/src/main/kotlin/arrow/meta/quotes/WhileExpression.kt
@@ -0,0 +1,35 @@
+package arrow.meta.quotes
+
+import arrow.meta.Meta
+import arrow.meta.phases.ExtensionPhase
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtWhileExpression
+
+/**
+ * A [KtWhileExpression] [Quote] with a custom template destructuring [WhileExpressionScope]
+ *
+ * @param match designed to to feed in any kind of [KtWhileExpression] predicate returning a [Boolean]
+ * @param map map a function that maps over the resulting action from matching on the transformation at the PSI level.
+ *
+ * ```kotlin
+ * whileExpression({ true }) { c ->
+ *   Transform.replace(
+ *     replacing = c,
+ *     newDeclaration = """ while $`(condition)` { $body } """.`while`
+ *   )
+ * }
+ *```
+ */
+fun Meta.whileExpression(
+  match: KtWhileExpression.() -> Boolean,
+  map: WhileExpressionScope.(KtWhileExpression) -> Transform<KtWhileExpression>
+): ExtensionPhase =
+  quote(match, map) { WhileExpressionScope(it) }
+
+/**
+ * A template destructuring [Scope] for a [KtWhileExpression]
+ */
+class WhileExpressionScope(
+  override val value: KtWhileExpression,
+  val `(condition)`: Scope<KtExpression> = Scope(value.condition)
+) : LoopExpressionScope<KtWhileExpression>(value)
