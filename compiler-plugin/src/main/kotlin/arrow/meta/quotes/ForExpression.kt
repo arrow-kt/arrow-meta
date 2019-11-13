7fa96c07d853a114e2c050dc6dff1e4426285a0c
Daniel Martins Leme <aptgetinstalldanielleme@gmail.com>
2019-11-13 13:25:13 -0300

    Add for quote example

--- compiler-plugin/src/main/kotlin/arrow/meta/quotes/ForExpression.kt
+++ compiler-plugin/src/main/kotlin/arrow/meta/quotes/ForExpression.kt
@@ -8,6 +8,16 @@ import org.jetbrains.kotlin.psi.KtForExpression
 
 /**
  * A [KtForExpression] [Quote] with a custom template destructuring [ForExpressionScope]
+ *
+ * @param match designed to to feed in any kind of [KtForExpression] predicate returning a [Boolean]
+ * @param map map a function that maps over the resulting action from matching on the transformation at the PSI level.
+ *
+ * forExpression({ true }) { e ->
+ *   Transform.replace(
+ *    replacing = e,
+ *    newDeclaration = """ for $`(param)` { $body } """.`for`
+ *   )
+ *  }
  */
 fun Meta.forExpression(
   match: KtForExpression.() -> Boolean,
@@ -22,5 +32,5 @@ class ForExpressionScope(
   override val value: KtForExpression,
   val `(param)`: ParameterScope = ParameterScope(value.loopParameter),
   val range: Scope<KtExpression> = Scope(value.loopRange),
-  val destructuringDeclaration: Scope<KtDestructuringDeclaration> = Scope(value.destructuringDeclaration)
+  val destructuringDeclaration: Scope<KtDestructuringDeclaration> = Scope(value.destructuringDeclaration) // TODO KtDestructuringDeclaration scope
 ) : LoopExpressionScope<KtForExpression>(value)
