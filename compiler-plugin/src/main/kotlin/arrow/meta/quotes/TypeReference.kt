f8bc32a33fb9f9e3798ed84e2b3f8a5d1d674877
Amanda Hinchman-Dominguez <ahinchman1@gmail.com>
2019-11-13 13:46:47 -0600

    updated documentation

--- compiler-plugin/src/main/kotlin/arrow/meta/quotes/TypeReference.kt
+++ compiler-plugin/src/main/kotlin/arrow/meta/quotes/TypeReference.kt
@@ -8,6 +8,11 @@ import org.jetbrains.kotlin.psi.KtTypeReference
 
 /**
  * A template destructuring [Scope] for a [KtTypeReference]
+ *
+ * Note: [KtTypeReference] is called [TypeMirror] in the Java annotation processing API.
+ *
+ * @param match designed to to feed in any kind of [KtTypeReference] predicate returning a [Boolean]
+ * @param map a function that maps over the resulting action from matching on the transformation at the PSI level.
  */
 fun Meta.typeReference(
   match: KtTypeReference.() -> Boolean,
@@ -22,4 +27,4 @@ class TypeReferenceScope(
   override val value: KtTypeReference,
   val typeElement: Scope<KtTypeElement>? = Scope(value.typeElement), // TODO KtTypeElement scope and quote template
   val `@annotations`: ScopedList<KtAnnotationEntry> = ScopedList(value.annotationEntries)
-  ): Scope<KtTypeReference>(value)
+): Scope<KtTypeReference>(value)
