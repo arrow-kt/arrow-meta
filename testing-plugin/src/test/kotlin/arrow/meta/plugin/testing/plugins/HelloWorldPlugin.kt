b10e35a8821593609f96c9a8c3382dbe1929f11f
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2019-11-22 23:03:04 +0100

    testing-plugin: allow to test Meta plugins (#217)

---
+++ testing-plugin/src/test/kotlin/arrow/meta/plugin/testing/plugins/HelloWorldPlugin.kt
@@ -0,0 +1,25 @@
+package arrow.meta.plugin.testing.plugins
+
+import arrow.meta.Meta
+import arrow.meta.Plugin
+import arrow.meta.invoke
+import arrow.meta.quotes.Transform
+import arrow.meta.quotes.namedFunction
+
+val Meta.helloWorld: Plugin
+  get() =
+    "Hello World" {
+      meta(
+        namedFunction({ name == "helloWorld" }) { c ->
+          Transform.replace(
+            replacing = c,
+            newDeclaration =
+            """| 
+               | fun helloWorld(): String = 
+               |   "Hello ΛRROW Meta!"
+               |   
+               |""".function.synthetic
+          )
+        }
+      )
+    }
