b18f9a9cdfcf8bbccf8e5bb8a5e33859f02875d1
i-walker <46971368+i-walker@users.noreply.github.com>
2019-12-03 16:24:49 +0100

    example

--- idea-plugin/src/main/kotlin/arrow/meta/ide/dsl/editor/documentation/DocumentationSyntax.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/dsl/editor/documentation/DocumentationSyntax.kt
@@ -18,6 +18,54 @@ interface DocumentationSyntax {
   /**
    * registers a [DocumentationProvider].
    * Users are now able to hover over descriptors and see the provided documentation.
+   * ```kotlin:ank:playground
+   * import arrow.meta.Plugin
+   * import arrow.meta.ide.IdeMetaPlugin
+   * import arrow.meta.invoke
+   * import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+   * import org.jetbrains.kotlin.idea.HtmlClassifierNamePolicy
+   * import org.jetbrains.kotlin.idea.WrapValueParameterHandler
+   * import org.jetbrains.kotlin.idea.caches.resolve.analyze
+   * import org.jetbrains.kotlin.psi.KtDeclaration
+   * import org.jetbrains.kotlin.renderer.AnnotationArgumentsRenderingPolicy
+   * import org.jetbrains.kotlin.renderer.ClassifierNamePolicy
+   * import org.jetbrains.kotlin.renderer.DescriptorRenderer
+   * import org.jetbrains.kotlin.resolve.BindingContext
+   * import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
+   * import org.jetbrains.kotlin.utils.addToStdlib.safeAs
+   *
+   * private val DeclarationRenderer: DescriptorRenderer = DescriptorRenderer.HTML.withOptions {
+   *  classifierNamePolicy = HtmlClassifierNamePolicy(ClassifierNamePolicy.SHORT)
+   *  valueParametersHandler = WrapValueParameterHandler(valueParametersHandler)
+   *  annotationArgumentsRenderingPolicy = AnnotationArgumentsRenderingPolicy.UNLESS_EMPTY
+   *  renderCompanionObjectName = true
+   *  withDefinedIn = false
+   *  eachAnnotationOnNewLine = true
+   *  boldOnlyForNamesInHtml = true
+   * }
+   *
+   * //sampleStart
+   * val IdeMetaPlugin.sampleQuickDocs: Plugin
+   *  get() = "QuickDocsDeclarations" {
+   *   meta(
+   *    addDocumentationProvider(
+   *     quickNavigateInfo = { element, originalElement ->
+   *      element.safeAs<KtDeclaration>()?.let { decl: KtDeclaration ->
+   *         decl.analyze(BodyResolveMode.PARTIAL).let { context: BindingContext ->
+   *          context[BindingContext.DECLARATION_TO_DESCRIPTOR, decl]?.let { descriptor: DeclarationDescriptor ->
+   *           DeclarationRenderer.render(descriptor) // this DeclarationRenderer pretty print's the descriptor
+   *          }
+   *         }
+   *        }
+   *       }
+   *      )
+   *     )
+   *    }
+   * //sampleEnd
+   * ```
+   * This example render's quickDocs on local declarations, but is insufficient for toplevel KtDeclarations.
+   * In addition, this implementation should not render the complete Doc's only the minimal amount of data.
+   * Hence, the KDoc's of this declaration should be added in [generateDoc].
    * @see quickDocumentationProvider
    */
   fun IdeMetaPlugin.addDocumentationProvider(
@@ -35,6 +83,7 @@ interface DocumentationSyntax {
    * @param quickNavigateInfo returns the short version of the Doc
    * @param generateDoc returns the entire Doc
    * @param documentationElementForLink given a [link] and a [context] this function resolves the PsiElement of that link
+   * @see [DocumentationProvider]
    */
   fun DocumentationSyntax.quickDocumentationProvider(
     quickNavigateInfo: (element: PsiElement, originalElement: PsiElement) -> String?,
--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/nothing/NothingIdePlugin.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/nothing/NothingIdePlugin.kt
@@ -1,20 +1,43 @@
 package arrow.meta.ide.plugins.nothing
 
 import arrow.meta.Plugin
-import arrow.meta.invoke
 import arrow.meta.ide.IdeMetaPlugin
-import arrow.meta.ide.resources.ArrowIcons
-import org.jetbrains.kotlin.psi.KtUserType
+import arrow.meta.invoke
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.idea.HtmlClassifierNamePolicy
+import org.jetbrains.kotlin.idea.WrapValueParameterHandler
+import org.jetbrains.kotlin.idea.caches.resolve.analyze
+import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.renderer.AnnotationArgumentsRenderingPolicy
+import org.jetbrains.kotlin.renderer.ClassifierNamePolicy
+import org.jetbrains.kotlin.renderer.DescriptorRenderer
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
-val IdeMetaPlugin.nothingIdePlugin: Plugin
-  get() = "NothingIdePlugin" {
+val IdeMetaPlugin.sampleQuickDocs: Plugin
+  get() = "QuickDocsDeclarations" {
     meta(
-      addLineMarkerProvider(
-        icon = ArrowIcons.NOTHING,
-        transform = { it.safeAs<KtUserType>()?.takeIf { type -> type.referencedName == "Nothing" }
-          ?.referenceExpression?.getReferencedNameElement()},
-        message = { "Bottom Type" }
+      addDocumentationProvider(
+        quickNavigateInfo = { element, originalElement ->
+          element.safeAs<KtDeclaration>()?.let { decl: KtDeclaration ->
+            decl.analyze(BodyResolveMode.PARTIAL).let { context: BindingContext ->
+              context[BindingContext.DECLARATION_TO_DESCRIPTOR, decl]?.let { descriptor: DeclarationDescriptor ->
+                DeclarationRenderer.render(descriptor)
+              }
+            }
+          }
+        }
       )
     )
   }
+
+private val DeclarationRenderer: DescriptorRenderer = DescriptorRenderer.HTML.withOptions {
+  classifierNamePolicy = HtmlClassifierNamePolicy(ClassifierNamePolicy.SHORT)
+  valueParametersHandler = WrapValueParameterHandler(valueParametersHandler)
+  annotationArgumentsRenderingPolicy = AnnotationArgumentsRenderingPolicy.UNLESS_EMPTY
+  renderCompanionObjectName = true
+  withDefinedIn = false
+  eachAnnotationOnNewLine = true
+  boldOnlyForNamesInHtml = true
+}
