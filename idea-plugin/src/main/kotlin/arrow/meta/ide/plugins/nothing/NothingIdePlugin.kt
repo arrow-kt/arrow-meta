f7cb1e3552ba9e18e1059a0fc168cda807aa0f3c
i-walker <46971368+i-walker@users.noreply.github.com>
2019-12-03 16:37:30 +0100

    cleanUp

--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/nothing/NothingIdePlugin.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/nothing/NothingIdePlugin.kt
@@ -1,43 +1,22 @@
 package arrow.meta.ide.plugins.nothing
 
 import arrow.meta.Plugin
-import arrow.meta.ide.IdeMetaPlugin
 import arrow.meta.invoke
-import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
-import org.jetbrains.kotlin.idea.HtmlClassifierNamePolicy
-import org.jetbrains.kotlin.idea.WrapValueParameterHandler
-import org.jetbrains.kotlin.idea.caches.resolve.analyze
-import org.jetbrains.kotlin.psi.KtDeclaration
-import org.jetbrains.kotlin.renderer.AnnotationArgumentsRenderingPolicy
-import org.jetbrains.kotlin.renderer.ClassifierNamePolicy
-import org.jetbrains.kotlin.renderer.DescriptorRenderer
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
+import arrow.meta.ide.IdeMetaPlugin
+import arrow.meta.ide.resources.ArrowIcons
+import org.jetbrains.kotlin.psi.KtUserType
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
-val IdeMetaPlugin.sampleQuickDocs: Plugin
-  get() = "QuickDocsDeclarations" {
+val IdeMetaPlugin.nothingIdePlugin: Plugin
+  get() = "NothingIdePlugin" {
     meta(
-      addDocumentationProvider(
-        quickNavigateInfo = { element, originalElement ->
-          element.safeAs<KtDeclaration>()?.let { decl: KtDeclaration ->
-            decl.analyze(BodyResolveMode.PARTIAL).let { context: BindingContext ->
-              context[BindingContext.DECLARATION_TO_DESCRIPTOR, decl]?.let { descriptor: DeclarationDescriptor ->
-                DeclarationRenderer.render(descriptor)
-              }
-            }
-          }
-        }
+      addLineMarkerProvider(
+        icon = ArrowIcons.NOTHING,
+        transform = {
+          it.safeAs<KtUserType>()?.takeIf { type -> type.referencedName == "Nothing" }
+            ?.referenceExpression?.getReferencedNameElement()
+        },
+        message = { "Bottom Type" }
       )
     )
   }
-
-private val DeclarationRenderer: DescriptorRenderer = DescriptorRenderer.HTML.withOptions {
-  classifierNamePolicy = HtmlClassifierNamePolicy(ClassifierNamePolicy.SHORT)
-  valueParametersHandler = WrapValueParameterHandler(valueParametersHandler)
-  annotationArgumentsRenderingPolicy = AnnotationArgumentsRenderingPolicy.UNLESS_EMPTY
-  renderCompanionObjectName = true
-  withDefinedIn = false
-  eachAnnotationOnNewLine = true
-  boldOnlyForNamesInHtml = true
-}
