37acc3081b395e22aee93059607565f7f6435135
i-walker <46971368+i-walker@users.noreply.github.com>
2019-12-07 09:57:33 +0100

    cleanUp

--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/helloworld/DummyPlugin.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/helloworld/DummyPlugin.kt
@@ -2,11 +2,13 @@ package arrow.meta.ide.plugins.helloworld
 
 import arrow.meta.Plugin
 import arrow.meta.ide.IdeMetaPlugin
+import arrow.meta.ide.dsl.editor.lineMarker.LineMarkerSyntax
 import arrow.meta.ide.resources.ArrowIcons
 import arrow.meta.invoke
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
-import arrow.meta.ide.dsl.editor.lineMarker.LineMarkerSyntax
+
+// TODO: Add Animation or example picture
 
 /**
  * The following section exemplifies a Hello World Ide Plugin
@@ -33,7 +35,6 @@ import arrow.meta.ide.dsl.editor.lineMarker.LineMarkerSyntax
  *
  * For every function with the name `helloWorld`, our ide plugin will register a lineMarker with our custom icon. And whenever
  * the user hovers over the Icon, it will display the message.
- * // TODO: Add Animation or example picture
  * @see [LineMarkerSyntax]
  */
 val IdeMetaPlugin.helloWorld: Plugin
