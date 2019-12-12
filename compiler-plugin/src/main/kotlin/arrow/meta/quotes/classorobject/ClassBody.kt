b97347818f8e48506cad1ab16a2dc62d06552483
Daniel Martins Leme <aptgetinstalldanielleme@gmail.com>
2019-12-12 01:48:32 -0300

    Create docs to class body scope

--- compiler-plugin/src/main/kotlin/arrow/meta/quotes/classorobject/ClassBody.kt
+++ compiler-plugin/src/main/kotlin/arrow/meta/quotes/classorobject/ClassBody.kt
@@ -10,6 +10,41 @@ import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtObjectDeclaration
 import org.jetbrains.kotlin.psi.KtProperty
 
+/**
+ * <code>"""{ $properties $companionObjects $functions }""".classBody</code>
+ *
+ * A template destructuring [Scope] for a [KtClassBody].
+ *
+ * ```kotlin:ank:silent
+ * import arrow.meta.Meta
+ * import arrow.meta.Plugin
+ * import arrow.meta.invoke
+ * import arrow.meta.quotes.Transform
+ * import arrow.meta.quotes.classBody
+ *
+ * val Meta.classBody: Plugin
+ *  get() =
+ *   "ReformatClassBody" {
+ *    meta(
+ *     classBody({ true }) { c ->
+ *      Transform.replace(
+ *       replacing = c,
+ *       newDeclaration =
+ *       """
+ *       | {
+ *       |  $properties
+ *       |
+ *       |  $companionObjects
+ *       |
+ *       |  $functions
+ *       | }
+ *       """.classBody
+ *      )
+ *     }
+ *    )
+ *   }
+ * ```
+ */
 class ClassBody(
   override val value: KtClassBody?,
   val companionObjects: ScopedList<KtObjectDeclaration> = ScopedList(value?.allCompanionObjects ?: listOf(), separator = "\n"),
