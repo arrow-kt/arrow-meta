1af0a5db303474b9ba8c0930eff70c7ba43d68aa
Matias Lev <matias.lev@globant.com>
2019-12-15 20:06:22 -0300

    unused import deleted

--- compiler-plugin/src/test/kotlin/arrow/meta/quotes/scope/ForExpressionTest.kt
+++ compiler-plugin/src/test/kotlin/arrow/meta/quotes/scope/ForExpressionTest.kt
@@ -5,13 +5,13 @@ import arrow.meta.plugin.testing.CompilerTest
 import arrow.meta.plugin.testing.CompilerTest.Companion.source
 import arrow.meta.plugin.testing.assertThis
 import arrow.meta.quotes.scope.plugins.ForExpressionPlugin
-import arrow.meta.quotes.scope.plugins.ReturnExpressionPlugin
 import io.kotlintest.specs.AnnotationSpec
+
 class ForExpressionTest : AnnotationSpec() {
 
-  @Test
-  fun `Validate for expression scope properties`() {
-    validate(""" 
+    @Test
+    fun `Validate for expression scope properties`() {
+        validate(""" 
                 | //metadebug
                 | 
                 | fun someFunction() {
@@ -20,20 +20,20 @@ class ForExpressionTest : AnnotationSpec() {
                 |   }
                 | }
                 | """.forExpression())
-  }
+    }
 
-  @Test
-  fun `Validate for expression single line scope properties`() {
-    validate("""
+    @Test
+    fun `Validate for expression single line scope properties`() {
+        validate("""
                   | fun singleLineFunction() {
                   |     for (i in 1..10) println(i)
                   | }
                   |   """.forExpression())
-  }
+    }
 
-  @Test
-  fun `Validate for expression destructuring declaration scope properties`() {
-    validate("""
+    @Test
+    fun `Validate for expression destructuring declaration scope properties`() {
+        validate("""
                   | fun destructuringDeclarationFunction() {
                   |         for ((index, value) in listOf("a", "b", "c").withIndex()) {
                   |              println("index: " + index)
@@ -41,23 +41,23 @@ class ForExpressionTest : AnnotationSpec() {
                   |         }
                   | }
                   |   """.forExpression())
-  }
+    }
 
-  fun validate(source: Code.Source) {
-    assertThis(CompilerTest(
+    fun validate(source: Code.Source) {
+        assertThis(CompilerTest(
             config = { listOf(addMetaPlugins(ForExpressionPlugin())) },
             code = { source },
             assert = { quoteOutputMatches(source) }
-    ))
-  }
+        ))
+    }
 
-  private fun String.forExpression(): Code.Source {
-    return """
+    private fun String.forExpression(): Code.Source {
+        return """
       | //metadebug
       | 
       | class Wrapper {
       |   $this
       | }
       | """.source
-  }
+    }
 }
