acdf29f1320c033b022bf37c91ec9131f50c68c7
matiaslev <39838885+matiaslev@users.noreply.github.com>
2019-12-16 05:57:08 -0300

    #340 ForExpression Quote and Template System validation (#342)
    * for expression test refactor and single line test implemented
    
    * ForExpressionTest with destructuring declaration implemented
    
    * unused import deleted

--- compiler-plugin/src/test/kotlin/arrow/meta/quotes/scope/ForExpressionTest.kt
+++ compiler-plugin/src/test/kotlin/arrow/meta/quotes/scope/ForExpressionTest.kt
@@ -1,5 +1,6 @@
 package arrow.meta.quotes.scope
 
+import arrow.meta.plugin.testing.Code
 import arrow.meta.plugin.testing.CompilerTest
 import arrow.meta.plugin.testing.CompilerTest.Companion.source
 import arrow.meta.plugin.testing.assertThis
@@ -8,22 +9,55 @@ import io.kotlintest.specs.AnnotationSpec
 
 class ForExpressionTest : AnnotationSpec() {
 
-  private val forExpression = """
-                         | //metadebug
-                         | 
-                         | fun someFunction() {
-                         |   for (i in 1..10) {
-                         |     println(i)
-                         |   }
-                         | }
-                         | """.source
+    @Test
+    fun `Validate for expression scope properties`() {
+        validate(""" 
+                | //metadebug
+                | 
+                | fun someFunction() {
+                |   for (i in 1..10) {
+                |     println(i)
+                |   }
+                | }
+                | """.forExpression())
+    }
 
-  @Test
-  fun `Validate for expression scope properties`() {
-    assertThis(CompilerTest(
-      config = { listOf(addMetaPlugins(ForExpressionPlugin())) },
-      code = { forExpression },
-      assert = { quoteOutputMatches(forExpression) }
-    ))
-  }
+    @Test
+    fun `Validate for expression single line scope properties`() {
+        validate("""
+                  | fun singleLineFunction() {
+                  |     for (i in 1..10) println(i)
+                  | }
+                  |   """.forExpression())
+    }
+
+    @Test
+    fun `Validate for expression destructuring declaration scope properties`() {
+        validate("""
+                  | fun destructuringDeclarationFunction() {
+                  |         for ((index, value) in listOf("a", "b", "c").withIndex()) {
+                  |              println("index: " + index)
+                  |              println("value: " + value)
+                  |         }
+                  | }
+                  |   """.forExpression())
+    }
+
+    fun validate(source: Code.Source) {
+        assertThis(CompilerTest(
+            config = { listOf(addMetaPlugins(ForExpressionPlugin())) },
+            code = { source },
+            assert = { quoteOutputMatches(source) }
+        ))
+    }
+
+    private fun String.forExpression(): Code.Source {
+        return """
+      | //metadebug
+      | 
+      | class Wrapper {
+      |   $this
+      | }
+      | """.source
+    }
 }
