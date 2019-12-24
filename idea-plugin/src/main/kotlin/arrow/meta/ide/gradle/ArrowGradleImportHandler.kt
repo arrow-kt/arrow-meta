668429708503e702d9abc87ddf92794307fe4ccd
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2019-12-24 19:05:16 +0100

    Add action to open Gradle configuration file

--- idea-plugin/src/main/kotlin/arrow/meta/ide/gradle/ArrowGradleImportHandler.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/gradle/ArrowGradleImportHandler.kt
@@ -7,12 +7,15 @@ import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.project.ModuleData
+import com.intellij.openapi.fileEditor.FileEditorManager
 import org.jetbrains.kotlin.idea.configuration.GradleProjectImportHandler
+import org.jetbrains.kotlin.idea.core.util.toVirtualFile
 import org.jetbrains.kotlin.idea.facet.KotlinFacet
 import org.jetbrains.plugins.gradle.model.GradleExtensions
 import org.jetbrains.plugins.gradle.model.data.GradleSourceSetData
 import java.awt.Toolkit
 import java.awt.datatransfer.StringSelection
+import java.nio.file.Paths
 
 const val notificationsEntry = "Arrow Meta Notifications"
 const val title = "Arrow Meta"
@@ -24,11 +27,24 @@ class ArrowGradleImportHandler : GradleProjectImportHandler {
     when (sourceSetNode.data.moduleName) {
       "main" ->
         if (arrowExtensions(sourceSetNode) == 0) {
-          val notificationContent = createNotificationContent((sourceSetNode.parent?.data as ModuleData).id)
+          val projectName = (sourceSetNode.parent?.data as ModuleData).moduleName
+          val notificationContent = createNotificationContent(projectName)
           val notification = Notification(notificationsEntry, title, notificationContent, NotificationType.INFORMATION)
           notification.addAction(object : NotificationAction("Copy code") {
             override fun actionPerformed(e: AnActionEvent, notification: Notification) {
               Toolkit.getDefaultToolkit().systemClipboard.setContents(StringSelection(arrowPlugin), null)
+            }
+          })
+          notification.addAction(object : NotificationAction("Open file") {
+            override fun actionPerformed(e: AnActionEvent, notification: Notification) {
+              val fileEditorManager = FileEditorManager.getInstance(facet.module.project)
+              val splitModuleName = facet.module.name.split(".")
+              val gradleConfPath = Paths.get(
+                facet.module.project.basePath,
+                *splitModuleName.subList(1, splitModuleName.size - 1).toTypedArray(),
+                "build.gradle"
+              )
+              gradleConfPath.toFile().toVirtualFile()?.let { fileEditorManager.openFile(it, true) }
             }
           })
           Notifications.Bus.notify(notification, facet.module.project)
