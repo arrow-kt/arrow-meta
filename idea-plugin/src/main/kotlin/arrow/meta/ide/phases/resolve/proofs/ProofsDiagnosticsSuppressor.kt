88f4981b778eefd69a2e9798fc77553753e0ad4d
Raul Raja <raulraja@gmail.com>
2020-01-02 19:24:12 +0100

    idea proof plugin clean up..

--- idea-plugin/src/main/kotlin/arrow/meta/ide/phases/resolve/proofs/ProofsDiagnosticsSuppressor.kt
+++
@@ -1,8 +0,0 @@
-package arrow.meta.ide.phases.resolve.proofs
-
-import org.jetbrains.kotlin.diagnostics.Diagnostic
-import org.jetbrains.kotlin.resolve.diagnostics.DiagnosticSuppressor
-
-class ProofsDiagnosticsSuppressor : DiagnosticSuppressor {
-  override fun isSuppressed(diagnostic: Diagnostic): Boolean = false
-}
