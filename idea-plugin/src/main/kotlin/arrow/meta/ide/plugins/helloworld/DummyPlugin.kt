beb542f2d528a2ea7037611c1e16d0cd73e7c7ba
i-walker <46971368+i-walker@users.noreply.github.com>
2020-01-31 15:00:44 +0100

    clickAction

--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/helloworld/DummyPlugin.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/helloworld/DummyPlugin.kt
@@ -6,7 +6,8 @@ import arrow.meta.ide.dsl.editor.lineMarker.LineMarkerSyntax
 import arrow.meta.ide.resources.ArrowIcons
 import arrow.meta.invoke
 import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptorIfAny
-import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.idea.util.hasInlineModifier
+import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 /**
@@ -35,20 +36,21 @@ import org.jetbrains.kotlin.utils.addToStdlib.safeAs
  * For every function with the name `helloWorld`, our ide plugin will register a lineMarker with our custom icon. And whenever
  * the user hovers over the Icon, it will display the message.
  * @see [LineMarkerSyntax]
+ * @sample [helloWorld]
  */
 val IdeMetaPlugin.helloWorld: Plugin // TODO: Add Animation or example picture
   get() = "Hello World" {
     meta(
       addLineMarkerProvider(
         icon = ArrowIcons.ICON1,
-        composite = KtNamedFunction::class.java,
-        message = { f: KtNamedFunction ->
-          HTML.renderMessage("Teach your users about this feature in function") + "<br/>" +
-            f.resolveToDescriptorIfAny()?.let(HTML::render)
+        composite = KtClass::class.java,
+        message = { ktClass: KtClass ->
+          HTML.renderMessage("Teach your users about this feature for inline classes") + "<br/>" +
+            ktClass.resolveToDescriptorIfAny()?.let(HTML::render)
         },
         transform = {
-          it.safeAs<KtNamedFunction>()?.takeIf { f ->
-            f.name == "helloWorld"
+          it.safeAs<KtClass>()?.takeIf { ktClass ->
+            ktClass.hasInlineModifier()
           }
         }
       )
