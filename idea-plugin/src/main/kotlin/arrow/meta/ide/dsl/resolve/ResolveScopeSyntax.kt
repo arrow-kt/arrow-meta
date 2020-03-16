527639c24cf1095a861913345645a0627c6e24d6
Matias Lev <matias.lev@globant.com>
2020-03-16 14:49:19 -0300

    comments - type signature and LoadingOrder updated

--- idea-plugin/src/main/kotlin/arrow/meta/ide/dsl/resolve/ResolveScopeSyntax.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/dsl/resolve/ResolveScopeSyntax.kt
@@ -2,7 +2,6 @@ package arrow.meta.ide.dsl.resolve
 
 import arrow.meta.ide.IdeMetaPlugin
 import arrow.meta.phases.ExtensionPhase
-import com.intellij.openapi.extensions.LoadingOrder
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.ResolveScopeEnlarger
@@ -10,14 +9,13 @@ import com.intellij.psi.search.SearchScope
 
 interface ResolveScopeSyntax {
     fun IdeMetaPlugin.resolveScope(
-        getAdditionalResolveScope: (file: VirtualFile, project: Project) -> SearchScope
+      additionalResolveScope: (file: VirtualFile, project: Project) -> SearchScope
     ): ExtensionPhase =
       extensionProvider(
         ResolveScopeEnlarger.EP_NAME,
         object : ResolveScopeEnlarger() {
-            override fun getAdditionalResolveScope(file: VirtualFile, project: Project?): SearchScope? =
-                getAdditionalResolveScope(file, project)
-        },
-        LoadingOrder.FIRST
+            override fun getAdditionalResolveScope(file: VirtualFile, project: Project): SearchScope =
+              additionalResolveScope(file, project)
+        }
       )
 }
