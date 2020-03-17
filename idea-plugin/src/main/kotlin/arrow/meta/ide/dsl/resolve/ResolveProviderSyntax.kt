94ac2d07b2a1b556381a8c2af1d0d2d86ad72a3c
ariostonjjunior <ariostonj@hotmail.com>
2020-03-16 23:47:05 -0300

    Add ResolveScopeProvider extension #480

---
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/dsl/resolve/ResolveProviderSyntax.kt
@@ -0,0 +1,36 @@
+package arrow.meta.ide.dsl.resolve
+
+import arrow.meta.ide.IdeMetaPlugin
+import arrow.meta.phases.ExtensionPhase
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.ResolveScopeProvider
+import com.intellij.psi.search.GlobalSearchScope
+
+interface ResolveProviderSyntax {
+    fun IdeMetaPlugin.getResolveScope(
+        getResolveScope: (file: VirtualFile, project: Project) -> GlobalSearchScope?
+    ): ExtensionPhase =
+            extensionProvider(
+                    ResolveScopeProvider.EP_NAME,
+                    object : ResolveScopeProvider(){
+                        override fun getResolveScope(file: VirtualFile, project: Project?): GlobalSearchScope? =
+                                getResolveScope(file, project)
+                    }
+            )
+
+    fun IdeMetaPlugin.globalSearchScope(
+        containsFile: (file: VirtualFile) -> Boolean,
+        isSearchInModuleContent: (aModule: Module) -> Boolean,
+        isSearchInLibraries: () -> Boolean
+    ): GlobalSearchScope =
+            object :  GlobalSearchScope(){
+                override fun contains(file: VirtualFile): Boolean = containsFile(file)
+
+                override fun isSearchInModuleContent(aModule: Module): Boolean = isSearchInModuleContent(aModule)
+
+                override fun isSearchInLibraries(): Boolean = isSearchInLibraries()
+
+            }
+}
