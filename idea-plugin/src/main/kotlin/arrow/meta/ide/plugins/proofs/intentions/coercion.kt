e78618c13ecd025bfd8142e5f42142c0020b37c4
Alberto Ballano <aballano@users.noreply.github.com>
2020-03-26 17:29:46 +0100

    Nits

--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/intentions/coercion.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/intentions/coercion.kt
@@ -24,8 +24,8 @@ fun IdeMetaPlugin.coercionIntention(): ExtensionPhase =
     kClass = KtElement::class.java,
     isApplicableTo = { ktCall: KtElement, caretOffset: Int ->
       when (ktCall) {
-        is KtProperty -> isPropertyCoerced(ktCall)
-        is KtDotQualifiedExpression -> isDotQualifiedExpressionCoerced(ktCall)
+        is KtDotQualifiedExpression -> ktCall.isDotQualifiedExpressionCoerced()
+        is KtProperty -> ktCall.isCoerced()
         else -> false
       }
     },
@@ -34,18 +34,18 @@ fun IdeMetaPlugin.coercionIntention(): ExtensionPhase =
     }
   )
 
-private fun isDotQualifiedExpressionCoerced(ktCall: KtDotQualifiedExpression): Boolean {
-  val bindingContext: BindingContext = ktCall.analyze(bodyResolveMode = BodyResolveMode.FULL)
+private fun KtDotQualifiedExpression.isDotQualifiedExpressionCoerced(): Boolean {
+  val bindingContext: BindingContext = analyze(bodyResolveMode = BodyResolveMode.FULL)
   val calls = bindingContext.getSliceContents(BindingContext.RESOLVED_CALL)
   return calls.filter { (call, resolvedCall) -> resolvedCall != null }
     .any { (call, resolvedCall) ->
-      ktCall.selectorExpression?.let { selectorExpression ->
-        val (type, superType) = ktCall.receiverExpression.resolveType() to selectorExpression.resolveType()
+      selectorExpression?.let { selectorExpression ->
+        val (type, superType) = receiverExpression.resolveType() to selectorExpression.resolveType()
         val isSubtypeOf = baseLineTypeChecker.isSubtypeOf(type, superType)
         val compilerContext = CompilerContext(project = call.callElement.project, eval = {
           KotlinJsr223StandardScriptEngineFactory4Idea().scriptEngine.eval(it)
         }).apply {
-          this.module = resolvedCall.resultingDescriptor.module
+          module = resolvedCall.resultingDescriptor.module
         }
         val proof = compilerContext.coerceProof(type, superType)
         !isSubtypeOf && proof != null
@@ -53,23 +53,23 @@ private fun isDotQualifiedExpressionCoerced(ktCall: KtDotQualifiedExpression): B
     }
 }
 
-private fun isPropertyCoerced(ktCall: KtProperty): Boolean {
-  val (supertype, subtype) = ktCall.type()!! to ktCall.initializer?.resolveType()!!
+private fun KtProperty.isCoerced(): Boolean {
+  val (supertype, subtype) = type()!! to initializer?.resolveType()!!
   val isSubtypeOf = baseLineTypeChecker.isSubtypeOf(subtype, supertype)
 
   if (isSubtypeOf) return false
 
-  return Log.Verbose({ "Found ${if (this) "" else "no"} proof for $supertype $subtype on ${ktCall.text}" }) {
-    val ctx = ktCall.analyze(bodyResolveMode = BodyResolveMode.FULL)
+  return Log.Verbose({ "Found ${if (this) "" else "no"} proof for $supertype $subtype on $text" }) {
+    val ctx = analyze(bodyResolveMode = BodyResolveMode.FULL)
     val calls = ctx.getSliceContents(BindingContext.RESOLVED_CALL)
 
-    // TODO Currently there are no calls for a property (?)
+    // TODO Currently there are no resolved calls for a property (?)
     val isProofSubtype = calls.filter { (call, resolvedCall) -> resolvedCall != null }
       .any { (call, resolvedCall) ->
         val compilerContext = CompilerContext(project = call.callElement.project, eval = {
           KotlinJsr223StandardScriptEngineFactory4Idea().scriptEngine.eval(it)
         }).apply {
-          this.module = resolvedCall.resultingDescriptor.module
+          module = resolvedCall.resultingDescriptor.module
         }
 
         compilerContext.coerceProof(subtype, supertype) != null
