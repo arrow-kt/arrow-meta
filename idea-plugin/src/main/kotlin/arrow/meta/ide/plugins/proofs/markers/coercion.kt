e5aad5db3e31598753d607c523c927afd5c0f173
danimontoya <daniel.montoya@47deg.com>
2020-03-31 11:29:30 +0200

    Update coercion line marker message with the proof applied (missing adding the actual link to the proof)

--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/markers/coercion.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/markers/coercion.kt
@@ -5,6 +5,7 @@ import arrow.meta.ide.plugins.proofs.intentions.participatingTypes
 import arrow.meta.phases.CompilerContext
 import arrow.meta.phases.ExtensionPhase
 import arrow.meta.plugins.proofs.phases.areTypesCoerced
+import arrow.meta.plugins.proofs.phases.coerceProof
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
@@ -17,7 +18,11 @@ fun IdeMetaPlugin.coerceProofLineMarker(icon: Icon, compilerContext: CompilerCon
     transform = { psiElement ->
       psiElement.safeAs<KtProperty>()?.takeIf { it.isCoerced(compilerContext) }
     },
-    message = { "Coercion happening by proof" }
+    message = { ktProperty: KtElement ->
+      ktProperty.participatingTypes()?.let { (subtype, supertype) ->
+        compilerContext.coerceProof(subtype, supertype)?.coercionMessage()
+      } ?: "Proof not found"
+    }
   )
 
 private fun KtElement.isCoerced(compilerContext: CompilerContext): Boolean {
