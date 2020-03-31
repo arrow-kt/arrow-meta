4ef911299eedfd96a12fca17a767ed9db018e080
Alberto Ballano <aballano@users.noreply.github.com>
2020-03-31 14:34:50 +0200

    WIP

--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/intentions/explicitcoercion.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/intentions/explicitcoercion.kt
@@ -17,38 +17,26 @@ fun IdeMetaPlugin.makeExplicitCoercionIntention(compilerContext: CompilerContext
     text = "Make coercion explicit",
     kClass = KtElement::class.java,
     isApplicableTo = { ktCall: KtElement, _ ->
-      ktCall.participatingTypes()?.let { (subtype, supertype) ->
+      ktCall.explicitParticipatingTypes()?.let { (subtype, supertype) ->
         compilerContext.areTypesCoerced(subtype, supertype)
       } ?: false
     },
     applyTo = { ktCall: KtElement, _ ->
-      TODO()
+//      TODO()
     }
   )
 
 //TODO move elsewhere
-fun KtElement.participatingTypes(): Pair<KotlinType, KotlinType>? =
+fun KtElement.explicitParticipatingTypes(): Pair<KotlinType, KotlinType>? =
   when (this) {
-    is KtCallElement -> {
-      //TODO implement for multiple args
-      val subType = valueArgumentList?.arguments?.get(0)?.getArgumentExpression()?.resolveType()
-      val superType = TODO() // extract expected argument types
-      subType toOrNull superType
-    }
 
     is KtDotQualifiedExpression ->
       receiverExpression.resolveType() toOrNull selectorExpression?.resolveType()
 
-    is KtProperty -> {
-      val superType = type()
-      val subType = initializer?.resolveType()
-      subType toOrNull superType
-
-    }
-
     else -> null
   }
 
-private infix fun <A, B> A?.toOrNull(b: B?): Pair<A, B>? =
+//TODO move elsewhere
+infix fun <A, B> A?.toOrNull(b: B?): Pair<A, B>? =
   if (this != null && b != null) this to b
   else null
--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/markers/coercion.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/markers/coercion.kt
@@ -1,13 +1,15 @@
 package arrow.meta.ide.plugins.proofs.markers
 
 import arrow.meta.ide.IdeMetaPlugin
-import arrow.meta.ide.plugins.proofs.intentions.participatingTypes
+import arrow.meta.ide.plugins.proofs.intentions.explicitParticipatingTypes
+import arrow.meta.ide.plugins.proofs.intentions.implicitParticipatingTypes
 import arrow.meta.phases.CompilerContext
 import arrow.meta.phases.ExtensionPhase
 import arrow.meta.plugins.proofs.phases.areTypesCoerced
 import arrow.meta.plugins.proofs.phases.coerceProof
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.types.KotlinType
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 import javax.swing.Icon
 
@@ -19,14 +21,17 @@ fun IdeMetaPlugin.coerceProofLineMarker(icon: Icon, compilerContext: CompilerCon
       psiElement.safeAs<KtProperty>()?.takeIf { it.isCoerced(compilerContext) }
     },
     message = { ktProperty: KtElement ->
-      ktProperty.participatingTypes()?.let { (subtype, supertype) ->
+      ktProperty.anyParticipatingTypes()?.let { (subtype, supertype) ->
         compilerContext.coerceProof(subtype, supertype)?.coercionMessage()
       } ?: "Proof not found"
     }
   )
 
+private fun KtElement.anyParticipatingTypes(): Pair<KotlinType, KotlinType>? =
+  explicitParticipatingTypes() ?: implicitParticipatingTypes()
+
 private fun KtElement.isCoerced(compilerContext: CompilerContext): Boolean {
-  return participatingTypes()?.let { (subtype, supertype) ->
+  return explicitParticipatingTypes()?.let { (subtype, supertype) ->
     compilerContext.areTypesCoerced(subtype, supertype)
   } ?: false
 }
