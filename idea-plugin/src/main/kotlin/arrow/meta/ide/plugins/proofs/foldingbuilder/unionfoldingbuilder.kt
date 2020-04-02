190da595d77160bc46bbfc45cf88a9fec0c86e55
danimontoya <daniel.montoya@47deg.com>
2020-04-02 19:57:15 +0200

    Trying to iterate through the Psi's

--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/foldingbuilder/unionfoldingbuilder.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/foldingbuilder/unionfoldingbuilder.kt
@@ -7,6 +7,10 @@ import com.intellij.lang.folding.FoldingBuilderEx
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
 import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtTypeReference
+import org.jetbrains.kotlin.psi.KtVisitor
+
 
 fun IdeMetaPlugin.addUnionFoldingBuilder(): ExtensionPhase =
   addFoldingBuilder(
@@ -30,11 +34,21 @@ class UnionFoldingBuilder : FoldingBuilderEx() {
 
   override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
     val result = arrayListOf<FoldingDescriptor>()
-    println("document=$document")
-    println("node=$root")
+
+    (root as KtElement).accept(object : KtVisitor<Boolean, Any?>() {
+      override fun visitTypeReference(typeReference: KtTypeReference, data: Any?): Boolean {
+        // TODO: check if type is Union and add it
+        result.add(FoldingDescriptor(typeReference, typeReference.textRange))
+        return super.visitTypeReference(typeReference, data)
+      }
+    }, null)
+
     return result.toTypedArray()
   }
 
   override fun isCollapsedByDefault(node: ASTNode): Boolean =
     true
+
+  private fun PsiElement.getFoldingDescriptor(): FoldingDescriptor? =
+    FoldingDescriptor(this, textRange)
 }
