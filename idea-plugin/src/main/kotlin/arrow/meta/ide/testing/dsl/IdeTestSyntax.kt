b0b3d54b12fb6e65513bd20c88cbe03241dbf09b
i-walker <46971368+i-walker@users.noreply.github.com>
2020-04-08 22:09:10 +0200

    inspection test dsl

--- idea-plugin/src/main/kotlin/arrow/meta/ide/testing/dsl/IdeTestSyntax.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/testing/dsl/IdeTestSyntax.kt
@@ -1,7 +1,8 @@
 package arrow.meta.ide.testing.dsl
 
 import arrow.meta.ide.testing.dsl.icon.IconProviderTestSyntax
+import arrow.meta.ide.testing.dsl.inspection.InspectionTestSyntax
 import arrow.meta.ide.testing.dsl.lineMarker.LineMarkerTestSyntax
 import arrow.meta.ide.testing.env.IdeTestTypeSyntax
 
-interface IdeTestSyntax : IdeTestTypeSyntax, LineMarkerTestSyntax, IconProviderTestSyntax
+interface IdeTestSyntax : IdeTestTypeSyntax, LineMarkerTestSyntax, IconProviderTestSyntax, InspectionTestSyntax
