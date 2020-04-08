c9ca5c9db080bc6fed6b8e90b06525c3a4a38b79
danimontoya <daniel.montoya@47deg.com>
2020-04-08 12:49:57 +0200

    Refactor to check for Union and Tuple type instead of Union or Tuple strings

--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/folding/tuples.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/folding/tuples.kt
@@ -1,6 +1,7 @@
 package arrow.meta.ide.plugins.proofs.folding
 
 import arrow.meta.ide.IdeMetaPlugin
+import arrow.meta.ide.dsl.utils.getType
 import arrow.meta.ide.dsl.utils.typeProjections
 import arrow.meta.ide.dsl.utils.typeReferences
 import arrow.meta.phases.ExtensionPhase
@@ -8,13 +9,15 @@ import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
 import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
+import org.jetbrains.kotlin.types.KotlinType
 
 val IdeMetaPlugin.codeFoldingOnTuples: ExtensionPhase
   get() = registerFoldingBuilder(
     foldingBuilder(
       placeHolderText = { node: ASTNode ->
         (node.psi as KtElement).typeProjections
-          .filter { !it.text.startsWith("Tuple") }
+          .filter { it.typeReference?.getType()?.isTupleType() == false }
           .map { it.text }
           .toString()
           .replace("[", "(")
@@ -22,10 +25,13 @@ val IdeMetaPlugin.codeFoldingOnTuples: ExtensionPhase
       },
       foldRegions = { node: ASTNode, _: Document ->
         (node.psi as KtElement).typeReferences
-          .filter { it.text.startsWith("Tuple") }
+          .filter { it.getType()?.isTupleType() == true }
           .map { FoldingDescriptor(it, it.textRange) }
           .toTypedArray()
       },
       isCollapsedByDefault = {
         true
       }))
+
+private fun KotlinType.isTupleType() =
+  constructor.declarationDescriptor?.fqNameSafe?.asString()?.startsWith("arrow.tuples.Tuple") == true
--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/folding/unionTypes.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/folding/unionTypes.kt
@@ -1,6 +1,7 @@
 package arrow.meta.ide.plugins.proofs.folding
 
 import arrow.meta.ide.IdeMetaPlugin
+import arrow.meta.ide.dsl.utils.getType
 import arrow.meta.ide.dsl.utils.typeProjections
 import arrow.meta.ide.dsl.utils.typeReferences
 import arrow.meta.phases.ExtensionPhase
@@ -8,13 +9,15 @@ import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
 import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
+import org.jetbrains.kotlin.types.KotlinType
 
 val IdeMetaPlugin.codeFoldingOnUnions: ExtensionPhase
   get() = registerFoldingBuilder(
     foldingBuilder(
       placeHolderText = { node: ASTNode ->
         (node.psi as KtElement).typeProjections
-          .filter { !it.text.startsWith("Union") }
+          .filter { it.typeReference?.getType()?.isUnionType() == false }
           .map { it.text }
           .toString()
           .replace("[", "")
@@ -23,10 +26,13 @@ val IdeMetaPlugin.codeFoldingOnUnions: ExtensionPhase
       },
       foldRegions = { node: ASTNode, _: Document ->
         (node.psi as KtElement).typeReferences
-          .filter { it.text.startsWith("Union") }
+          .filter { it.getType()?.isUnionType() == true }
           .map { FoldingDescriptor(it, it.textRange) }
           .toTypedArray()
       },
       isCollapsedByDefault = {
         true
       }))
+
+private fun KotlinType.isUnionType() =
+  constructor.declarationDescriptor?.fqNameSafe?.asString() == "arrow.Union22"
