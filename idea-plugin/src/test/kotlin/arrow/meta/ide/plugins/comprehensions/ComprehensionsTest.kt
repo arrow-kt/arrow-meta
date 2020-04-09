958a0acf46e4f46f17e788cc1ccd0a9bc7fb3ec9
i-walker <46971368+i-walker@users.noreply.github.com>
2020-04-09 11:13:12 +0200

    fix tests

--- idea-plugin/src/test/kotlin/arrow/meta/ide/plugins/comprehensions/ComprehensionsTest.kt
+++ idea-plugin/src/test/kotlin/arrow/meta/ide/plugins/comprehensions/ComprehensionsTest.kt
@@ -1,75 +1,75 @@
 package arrow.meta.ide.plugins.comprehensions
 
+import arrow.meta.ide.IdeMetaPlugin
 import arrow.meta.ide.resources.ArrowIcons
 import arrow.meta.ide.testing.IdeTest
+import arrow.meta.ide.testing.dsl.lineMarker.LineMarkerDescription
 import arrow.meta.ide.testing.env.IdeTestSetUp
 import arrow.meta.ide.testing.env.ideTest
-import arrow.meta.ide.testing.resolves
 import org.junit.Test
 
 class ComprehensionsTest : IdeTestSetUp() {
   @Test
   fun `ComprehensionsTest for LineMarkers`(): Unit =
     ideTest(
-      IdeTest(
-        code = ComprehensionsTestCode.code1,
-        myFixture = myFixture,
-        test = { code, myFixture ->
-          collectLM(code, myFixture, ArrowIcons.BIND)
-        },
-        result = resolves("LineMarkerTest for two LM on typed variables") {
-          it.takeIf { descriptor -> descriptor.lineMarker.size == 2 && descriptor.slowLM.isEmpty() }
-        }
-      ),
-      IdeTest(
-        code = ComprehensionsTestCode.code2,
-        myFixture = myFixture,
-        test = { code, myFixture ->
-          collectLM(code, myFixture, ArrowIcons.BIND)
-        },
-        result = resolves("LineMarkerTest for two LM on untyped variables") {
-          it.takeIf { descriptor -> descriptor.lineMarker.size == 2 && descriptor.slowLM.isEmpty() }
-        }
-      ),
-      IdeTest(
-        code = ComprehensionsTestCode.code3,
-        myFixture = myFixture,
-        test = { code, myFixture ->
-          collectLM(code, myFixture, ArrowIcons.BIND)
-        },
-        result = resolves("LineMarkerTest for six LM on untyped variables") {
-          it.takeIf { descriptor -> descriptor.lineMarker.size == 6 && descriptor.slowLM.isEmpty() }
-        }
-      ),
-      IdeTest(
-        code = ComprehensionsTestCode.code4,
-        myFixture = myFixture,
-        test = { code, myFixture ->
-          collectLM(code, myFixture, ArrowIcons.BIND)
-        },
-        result = resolves("LineMarkerTest for four LM on untyped variables") {
-          it.takeIf { descriptor -> descriptor.lineMarker.size == 4 && descriptor.slowLM.isEmpty() }
-        }
-      ),
-      IdeTest(
-        code = ComprehensionsTestCode.code5,
-        myFixture = myFixture,
-        test = { code, myFixture ->
-          collectLM(code, myFixture, ArrowIcons.BIND)
-        },
-        result = resolves("LineMarkerTest for zero LM ") {
-          it.takeIf { descriptor -> descriptor.lineMarker.isEmpty() && descriptor.slowLM.isEmpty() }
-        }
-      ),
-      IdeTest(
-        code = ComprehensionsTestCode.code6,
-        myFixture = myFixture,
-        test = { code, myFixture ->
-          collectLM(code, myFixture, ArrowIcons.BIND)
-        },
-        result = resolves("LineMarkerTest for no LM ") {
-          it.takeIf { descriptor -> descriptor.lineMarker.isEmpty() && descriptor.slowLM.isEmpty() }
-        }
+      myFixture = myFixture,
+      ctx = IdeMetaPlugin()
+    ) {
+      listOf<IdeTest<LineMarkerDescription, IdeMetaPlugin>>(
+        IdeTest(
+          code = ComprehensionsTestCode.code1,
+          test = { code, myFixture, _ ->
+            collectLM(code, myFixture, ArrowIcons.BIND)
+          },
+          result = resolvesWith("LineMarkerTest for two LM on typed variables") {
+            it.takeIf { descriptor -> descriptor.lineMarker.size == 2 && descriptor.slowLM.isEmpty() }
+          }
+        ),
+        IdeTest(
+          code = ComprehensionsTestCode.code2,
+          test = { code, myFixture, _ ->
+            collectLM(code, myFixture, ArrowIcons.BIND)
+          },
+          result = resolvesWith("LineMarkerTest for two LM on untyped variables") {
+            it.takeIf { descriptor -> descriptor.lineMarker.size == 2 && descriptor.slowLM.isEmpty() }
+          }
+        ),
+        IdeTest(
+          code = ComprehensionsTestCode.code3,
+          test = { code, myFixture, _ ->
+            collectLM(code, myFixture, ArrowIcons.BIND)
+          },
+          result = resolvesWith("LineMarkerTest for six LM on untyped variables") {
+            it.takeIf { descriptor -> descriptor.lineMarker.size == 6 && descriptor.slowLM.isEmpty() }
+          }
+        ),
+        IdeTest(
+          code = ComprehensionsTestCode.code4,
+          test = { code, myFixture, _ ->
+            collectLM(code, myFixture, ArrowIcons.BIND)
+          },
+          result = resolvesWith("LineMarkerTest for four LM on untyped variables") {
+            it.takeIf { descriptor -> descriptor.lineMarker.size == 4 && descriptor.slowLM.isEmpty() }
+          }
+        ),
+        IdeTest(
+          code = ComprehensionsTestCode.code5,
+          test = { code, myFixture, _ ->
+            collectLM(code, myFixture, ArrowIcons.BIND)
+          },
+          result = resolvesWith("LineMarkerTest for zero LM ") {
+            it.takeIf { descriptor -> descriptor.lineMarker.isEmpty() && descriptor.slowLM.isEmpty() }
+          }
+        ),
+        IdeTest(
+          code = ComprehensionsTestCode.code6,
+          test = { code, myFixture, _ ->
+            collectLM(code, myFixture, ArrowIcons.BIND)
+          },
+          result = resolvesWith("LineMarkerTest for no LM ") {
+            it.takeIf { descriptor -> descriptor.lineMarker.isEmpty() && descriptor.slowLM.isEmpty() }
+          }
+        )
       )
-    )
+    }
 }
--- idea-plugin/src/test/kotlin/arrow/meta/ide/plugins/higherkinds/HigherKindTest.kt
+++ idea-plugin/src/test/kotlin/arrow/meta/ide/plugins/higherkinds/HigherKindTest.kt
@@ -1,61 +1,68 @@
 package arrow.meta.ide.plugins.higherkinds
 
+import arrow.meta.ide.IdeMetaPlugin
 import arrow.meta.ide.resources.ArrowIcons
 import arrow.meta.ide.testing.IdeTest
+import arrow.meta.ide.testing.dsl.lineMarker.LineMarkerDescription
 import arrow.meta.ide.testing.env.IdeTestSetUp
 import arrow.meta.ide.testing.env.ideTest
-import arrow.meta.ide.testing.resolves
 import org.junit.Test
 
 class HigherKindTest : IdeTestSetUp() {
   @Test
-  fun `HKT Tests for LineMarkers`() =
+  fun `HKT lineMarker tests`(): Unit =
     ideTest(
-      IdeTest(
-        code = HigherKindsTestCode.code1,
-        myFixture = myFixture,
-        test = { code, myFixture ->
-          collectLM(code, myFixture, ArrowIcons.HKT)
-        },
-        result = resolves("LineMarkerTest for one valid HKT") {
-          it.takeIf { description -> description.lineMarker.size == 1 && description.slowLM.isEmpty() }
-        }
-      ),
-      IdeTest(
-        code = HigherKindsTestCode.code2,
-        myFixture = myFixture,
-        test = { code, myFixture ->
-          collectLM(code, myFixture, ArrowIcons.HKT)
-        },
-        result = resolves("LineMarkerTest for multiple valid HKT") {
-          it.takeIf { description -> description.lineMarker.size == 2 && description.slowLM.isEmpty() }
-        }
-      ),
-      IdeTest(
-        code = HigherKindsTestCode.code3,
-        myFixture = myFixture,
-        test = { code, myFixture ->
-          collectLM(code, myFixture, ArrowIcons.HKT)
-        },
-        result = resolves("LineMarkerTest for no valid HKT") {
-          it.takeIf { description -> description.lineMarker.isEmpty() && description.slowLM.isEmpty() }
-        }
+      myFixture = myFixture,
+      ctx = IdeMetaPlugin()
+    ) {
+      listOf<IdeTest<LineMarkerDescription, IdeMetaPlugin>>(
+        IdeTest(
+          code = HigherKindsTestCode.code1,
+          test = { code, myFixture, _ ->
+            collectLM(code, myFixture, ArrowIcons.HKT)
+          },
+          result = resolvesWith("LineMarkerTest for one valid HKT") {
+            it.takeIf { description -> description.lineMarker.size == 1 && description.slowLM.isEmpty() }
+          }
+        ),
+        IdeTest(
+          code = HigherKindsTestCode.code2,
+          test = { code, myFixture, _ ->
+            collectLM(code, myFixture, ArrowIcons.HKT)
+          },
+          result = resolvesWith("LineMarkerTest for multiple valid HKT") {
+            it.takeIf { description -> description.lineMarker.size == 2 && description.slowLM.isEmpty() }
+          }
+        ),
+        IdeTest(
+          code = HigherKindsTestCode.code3,
+          test = { code, myFixture, _ ->
+            collectLM(code, myFixture, ArrowIcons.HKT)
+          },
+          result = resolvesWith("LineMarkerTest for no valid HKT") {
+            it.takeIf { description -> description.lineMarker.isEmpty() && description.slowLM.isEmpty() }
+          }
+        )
       )
-    )
+    }
 
 
   @Test
-  fun `HKT Tests for Poly LineMarkers`() =
+  fun `HKT poly lineMarker test`() =
     ideTest(
-      IdeTest(
-        code = HigherKindsTestCode.code4,
-        myFixture = myFixture,
-        test = { code, myFixture ->
-          collectLM(code, myFixture, ArrowIcons.POLY)
-        },
-        result = resolves("LineMarkerTest for Poly Icon") {
-          it.takeIf { description -> description.lineMarker.size == 2  && description.slowLM.isEmpty() }
-        }
+      myFixture = myFixture,
+      ctx = IdeMetaPlugin()
+    ) {
+      listOf<IdeTest<LineMarkerDescription, IdeMetaPlugin>>(
+        IdeTest(
+          code = HigherKindsTestCode.code4,
+          test = { code, myFixture, _ ->
+            collectLM(code, myFixture, ArrowIcons.POLY)
+          },
+          result = resolvesWith("LineMarkerTest for Poly Icon") {
+            it.takeIf { description -> description.lineMarker.size == 2 && description.slowLM.isEmpty() }
+          }
+        )
       )
-    )
+    }
 }
