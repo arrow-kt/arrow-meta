573c9bc82bc142f6d723168d92d301573dd36273
danimontoya <daniel.montoya@47deg.com>
2020-04-28 16:30:04 +0200

    Change indentation

--- compiler-plugin/src/test/kotlin/arrow/meta/plugins/refinement/RefinementTests.kt
+++ compiler-plugin/src/test/kotlin/arrow/meta/plugins/refinement/RefinementTests.kt
@@ -17,18 +17,18 @@ class RefinementTests {
     """
     @Refinement
     inline class TwitterHandle(val handle: String) {
-        companion object : Refined<String, TwitterHandle> {
-            override val target = ::TwitterHandle
-            override val validate: String.() -> Map<String, Boolean> = {
-                mapOf(
-                    "Should start with '@'" to startsWith("@"),
-                    "Should have length <= 16" to (length <= 16),
-                    "Should have length > 2" to (length > 2),
-                    "Should not contain the word 'twitter'" to !contains("twitter"),
-                    "Should not contain the word 'admin'" to !contains("admin")
-                )
-            }
+      companion object : Refined<String, TwitterHandle> {
+        override val target = ::TwitterHandle
+        override val validate: String.() -> Map<String, Boolean> = {
+          mapOf(
+            "Should start with '@'" to startsWith("@"),
+            "Should have length <= 16" to (length <= 16),
+            "Should have length > 2" to (length > 2),
+            "Should not contain the word 'twitter'" to !contains("twitter"),
+            "Should not contain the word 'admin'" to !contains("admin")
+          )
         }
+      }
     }
     
     @Coercion
