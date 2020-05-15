6b84bb19f6ebb9d8f5073343f67ec4ece72c7168
J. Ansorg <github@joachim-ansorg.de>
2020-05-15 17:54:57 +0200

    Update Gradle test framework to 2020.1 (#625)
    * Update our copy of JetBrains's gradle test runner integration with the files of the 2020.1 branch.
    Staying closer to the original sources, noted changes in Readme.txt files.
    Removed currently unused JPS related files.
    
    * Fix conf for Kotlin 1.3.61 and Idea 2020.1.1 (#627)
    
    Co-authored-by: Rachel M. Carmena <rachelcarmena@users.noreply.github.com>

--- idea-plugin/src/test/java/org/jetbrains/plugins/gradle/compiler/GradleCompilingTestCase.java
+++ idea-plugin/src/test/java/org/jetbrains/plugins/gradle/compiler/GradleCompilingTestCase.java
@@ -1,11 +1,12 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package external.forks.jb.gradle;
+package org.jetbrains.plugins.gradle.compiler;
 
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VfsUtilCore;
+import org.jetbrains.plugins.gradle.importing.GradleImportingTestCase;
 
 import java.io.File;
 
