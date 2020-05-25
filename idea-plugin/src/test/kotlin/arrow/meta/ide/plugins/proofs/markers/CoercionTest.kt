3f5f329a4bcedf010b72975f97b87b4a4d417d5b
danimontoya <daniel.montoya@47deg.com>
2020-05-25 13:46:26 +0200

    Checking if error comes from CoercionTest

--- idea-plugin/src/test/kotlin/arrow/meta/ide/plugins/proofs/markers/CoercionTest.kt
+++ idea-plugin/src/test/kotlin/arrow/meta/ide/plugins/proofs/markers/CoercionTest.kt
@@ -8,6 +8,7 @@ import arrow.meta.ide.testing.dsl.lineMarker.LineMarkerDescription
 import arrow.meta.ide.testing.env.IdeTestSetUp
 import arrow.meta.ide.testing.env.ideTest
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import org.junit.Ignore
 
 class CoercionTest : IdeTestSetUp() {
 
@@ -17,6 +18,7 @@ class CoercionTest : IdeTestSetUp() {
     myFixture.addFileToProject("consumer/consumer.kt", CoercionTestCode.twitterHandleDeclaration)
   }
 
+  @Ignore
   @org.junit.Test
   fun `test coercion line marker`() =
     ideTest(
