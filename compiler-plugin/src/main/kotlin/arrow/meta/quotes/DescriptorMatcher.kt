cd6ec0402a6a5bbef99ab76bccef8968113bb06f
Bloder <aptgetinstalldanielleme@gmail.com>
2020-06-27 21:47:28 -0300

    Create type class descriptors test

--- compiler-plugin/src/main/kotlin/arrow/meta/quotes/DescriptorMatcher.kt
+++ compiler-plugin/src/main/kotlin/arrow/meta/quotes/DescriptorMatcher.kt
@@ -1,6 +1,5 @@
 package arrow.meta.quotes
 
-import org.jetbrains.kotlin.codegen.coroutines.createCustomCopy
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.PropertyDescriptor
@@ -9,20 +8,18 @@ import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.KtTypeAlias
 
-interface DescriptorQuote<D : DeclarationDescriptor> {
+interface DescriptorMatcher<D : DeclarationDescriptor> {
 
   fun asDescriptor(): D?
 }
 
-private fun <D : DeclarationDescriptor> descriptorQuote(transform: () -> D?): DescriptorQuote<D> = object : DescriptorQuote<D> {
+private fun <D : DeclarationDescriptor> descriptorQuote(transform: () -> D?): DescriptorMatcher<D> = object : DescriptorMatcher<D> {
   override fun asDescriptor(): D? = transform()
 }
 
 fun List<DeclarationDescriptor>.namedFunctionDescriptor(element: KtNamedFunction): FunctionDescriptor? = descriptorQuote {
   this.firstOrNull { (it as? FunctionDescriptor)?.let {
-    it.isOperator
     it.name == element.nameAsSafeName
-    && it.returnType?.toString() == element.typeReference?.text
     && it.valueParameters.zip(element.valueParameters) { descriptor, element -> descriptor.type.toString() == element.typeReference?.text }.none { !it }
   } == true } as? FunctionDescriptor
 }.asDescriptor()
