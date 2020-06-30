ed82819ac483d32cd75ae767cbf74670f0ac05be
danimontoya <daniel.montoya@47deg.com>
2020-06-30 11:33:14 +0200

    Add compilerContext parameter Add Fixme for shadow library

--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/markers/ProofMarkers.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/markers/ProofMarkers.kt
@@ -1,8 +1,6 @@
 package arrow.meta.ide.plugins.proofs.markers
 
 import arrow.meta.ide.IdeMetaPlugin
-import arrow.meta.ide.plugins.proofs.psi.proof
-import arrow.meta.ide.plugins.proofs.psi.returnTypeCallableMembers
 import arrow.meta.internal.Noop
 import arrow.meta.phases.CompilerContext
 import arrow.meta.phases.ExtensionPhase
@@ -16,9 +14,8 @@ import arrow.meta.plugins.proofs.phases.ProjectionProof
 import arrow.meta.plugins.proofs.phases.Proof
 import arrow.meta.plugins.proofs.phases.RefinementProof
 import arrow.meta.quotes.scope
-import com.intellij.openapi.util.text.StringUtil
 import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptorIfAny
-import org.jetbrains.kotlin.idea.refactoring.pullUp.renderForConflicts
+import com.intellij.openapi.util.text.StringUtil
 import org.jetbrains.kotlin.psi.KtDeclaration
 import org.jetbrains.kotlin.psi.KtNamedDeclaration
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
@@ -33,17 +30,20 @@ inline fun <reified A : KtNamedDeclaration> IdeMetaPlugin.proofLineMarkers(icon:
     },
     composite = KtNamedDeclaration::class.java,
     message = { decl: KtNamedDeclaration ->
-      decl.markerMessage()
+      decl.ctx()?.let { ctx: CompilerContext ->
+        decl.markerMessage(ctx)
+      } ?: "TODO"
     }
   )
 
-fun KtDeclaration.markerMessage(): String =
-  proof {
+fun KtDeclaration.markerMessage(ctx: CompilerContext): String =
+  proof(ctx) {
     StringUtil.escapeXmlEntities(it.description().trimIndent())
   }.orEmpty()
 
-fun <A> KtDeclaration.proof(f: (Proof) -> A): A? =
-  scope().value?.resolveToDescriptorIfAny(bodyResolveMode = BodyResolveMode.PARTIAL)?.proof()?.let(f)
+fun <A> KtDeclaration.proof(ctx: CompilerContext, f: (Proof) -> A): A? = null
+  // FIXME: fix shadows so KtDeclaration.proof(ctx: CompilerContext, f: (Proof) -> ???): ??? can be resolved correctly
+  //scope().value?.resolveToDescriptorIfAny(bodyResolveMode = BodyResolveMode.PARTIAL)?.proof(ctx)?.let(f)
 
 fun Proof.description(): String =
   fold(
@@ -64,97 +64,3 @@ fun Proof.description(): String =
       """$from can be used in place of $to as if $to : $from, all members of $to are available as members of $from"""
     }
   ).orEmpty()
-
-fun Proof.markerMessage(): String =
-  when (this) {
-    is ClassProof -> markerMessage()
-    is ObjectProof -> markerMessage()
-    is CallableMemberProof -> markerMessage()
-    is CoercionProof -> markerMessage()
-    is ProjectionProof -> markerMessage()
-    is RefinementProof -> markerMessage()
-  }
-
-fun GivenProof.markerMessage(): String {
-  return """
-  TODO() Given proof
-  """.trimIndent()
-}
-
-fun RefinementProof.markerMessage(): String {
-  return """
-        <code lang="kotlin">$from</code> is a refined type that represents a set of values of the type <code lang="kotlin">$to</code>
-        that meet a certain type-level predicate.
-        ```
-  """.trimIndent()
-}
-
-fun ExtensionProof.markerMessage(): String {
-  return """
-  All members of <code lang="kotlin">$to</code> become available in <code lang="kotlin">$from</code> :
-  <ul>
-  ${through.returnTypeCallableMembers().joinToString("\n -") {
-    """
-            <li>${it.renderForConflicts()}</li>
-            """.trimIndent()
-  }}
-  </ul>
-  <code lang="kotlin">$from</code> does not need to explicitly extend <code lang="kotlin">$to</code>, instead <code lang="kotlin">${through.name}</code>
-  is used as proof to support the intersection of <code lang="kotlin">$from & $to</code>.
-  """.trimIndent()
-}
-
-fun ExtensionProof.subtypingMarkerMessage(): String {
-  return """
-        <code lang="kotlin">$from</code> is seen as subtype of <code lang="kotlin">$to</code> :
-        
-        <code lang="kotlin">
-        $from : $to
-        </code>
-        ```
-        
-        <code lang="kotlin">$from</code> does not need to explicitly extend <code lang="kotlin">$to</code>, instead <code lang="kotlin">${through.name}</code> 
-        is used as injective function proof to support all subtype associations of <code lang="kotlin">$from : $to</code>.
-        
-        <code lang="kotlin">
-        val a: $from = TODO()
-        val b: $to = a //ok
-        </code>
-        
-        In the example above compiling <code lang="kotlin">val b: $to = a</code> would have failed to compile but because we have proof of 
-        <code lang="kotlin">$from : $to</code> this becomes a valid global ad-hoc synthetic subtype relationship.
-        """.trimIndent()
-}
-
-fun KtDeclaration.markerMessage(ctx: CompilerContext): String =
-  scope().run {
-    value?.resolveToDescriptorIfAny(bodyResolveMode = BodyResolveMode.PARTIAL)?.proof(ctx)?.let { proof ->
-      val message = proof.markerMessage()
-      """
-      <code lang="kotlin">${text}</code> 
-      $message
-    """.trimIndent()
-    }.orEmpty()
-  }
-
-inline fun <reified A : KtDeclaration> IdeMetaPlugin.proofLineMarkers(icon: Icon, crossinline filter: A.() -> Boolean): ExtensionPhase =
-  addLineMarkerProvider(
-    icon = icon,
-    transform = {
-      it.safeAs<A>()?.takeIf(filter)
-    },
-    composite = KtDeclaration::class.java,
-    message = {
-      it.ctx()?.let {ctx ->
-        it.markerMessage(ctx)
-      } ?: "TODO"
-    }
-  )
-
-fun CoercionProof.markerMessage(): String =
-  """
-    Coercion happening by proof:
-    <code lang="kotlin">$from</code> is not a subtype of <code lang="kotlin">$to</code>.. but there is a proof to go from: <code lang="kotlin">$from</code> to <code lang="kotlin">$to</code> :
-    Link to proof declaration:
-    <code lang="kotlin">$through</code>
-  """.trimIndent()
