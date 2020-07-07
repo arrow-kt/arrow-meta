f80141c8b2a2f2a63c842e367b9a1b943ab68cba
danimontoya <daniel.montoya@47deg.com>
2020-07-07 11:30:28 +0200

    Add truncate mechanism for tuples

--- idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/folding/tuples.kt
+++ idea-plugin/src/main/kotlin/arrow/meta/ide/plugins/proofs/folding/tuples.kt
@@ -2,7 +2,9 @@ package arrow.meta.ide.plugins.proofs.folding
 
 import arrow.meta.ide.IdeMetaPlugin
 import arrow.meta.phases.ExtensionPhase
+import com.intellij.psi.PsiElement
 import com.intellij.psi.util.strictParents
+import org.jetbrains.kotlin.psi.KtTypeArgumentList
 import org.jetbrains.kotlin.psi.KtTypeProjection
 import org.jetbrains.kotlin.psi.KtTypeReference
 import org.jetbrains.kotlin.psi.KtUserType
@@ -26,17 +28,24 @@ private fun KotlinType?.isTypeMatching() =
   this?.constructor?.declarationDescriptor?.fqNameSafe?.asString()?.startsWith("arrow.tuples.Tuple") ?: false
 
 private fun KtTypeReference.foldString(): String =
-  firstChild.safeAs<KtUserType>()?.typeArgumentList?.let {
-    it.children.joinToString(
-      prefix = "(",
-      postfix = ")",
-      transform = {
-        it.safeAs<KtTypeProjection>()?.typeReference?.let { ktTypeReference ->
+  firstChild.safeAs<KtUserType>()?.typeArgumentList?.let { ktTypeArgList: KtTypeArgumentList ->
+    ktTypeArgList.children.joinToString(
+      separator = ",",
+      transform = { psi: PsiElement ->
+        psi.safeAs<KtTypeProjection>()?.typeReference?.let { ktTypeReference ->
           if (ktTypeReference.getType().isTypeMatching()) {
             ktTypeReference.foldString()
           } else {
             ktTypeReference.text
           }
-        } ?: it.text
+        } ?: psi.text
       })
+      .split(",")
+      .joinToString(
+        prefix = "(",
+        postfix = ")",
+        separator = ", ",
+        limit = 5,
+        truncated = "..."
+      )
   } ?: text
--- prelude/src/main/kotlin/arrow/tuples/tuples.kt
+++ prelude/src/main/kotlin/arrow/tuples/tuples.kt
@@ -1,6 +1,7 @@
 @file:Suppress("NON_PUBLIC_PRIMARY_CONSTRUCTOR_OF_INLINE_CLASS", "UNCHECKED_CAST")
 
 package arrow.tuples
+
 /**
  * Products                                   _              _
  *                                      _.(_)..__________(_)
@@ -51,7 +52,32 @@ interface Tuple4<out A, out B, out C, out D> : Tuple3<A, B, C> {
   operator fun component4(): D = value[3] as D
 }
 
-inline class tupleOf private constructor(override val value: Array<Any?>) : Tuple4<Nothing, Nothing, Nothing, Nothing> {
+interface Tuple5<out A, out B, out C, out D, out E> : Tuple4<A, B, C, D> {
+  operator fun component5(): E = value[4] as E
+}
+
+interface Tuple6<out A, out B, out C, out D, out E, out F> : Tuple5<A, B, C, D, E> {
+  operator fun component6(): F = value[5] as F
+}
+
+interface Tuple7<out A, out B, out C, out D, out E, out F, out G> : Tuple6<A, B, C, D, E, F> {
+  operator fun component7(): G = value[6] as G
+}
+
+interface Tuple8<out A, out B, out C, out D, out E, out F, out G, out H> : Tuple7<A, B, C, D, E, F, G> {
+  operator fun component8(): H = value[7] as H
+}
+
+interface Tuple9<out A, out B, out C, out D, out E, out F, out G, out H, out I> : Tuple8<A, B, C, D, E, F, G, H> {
+  operator fun component9(): I = value[8] as I
+}
+
+interface Tuple10<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J> : Tuple9<A, B, C, D, E, F, G, H, I> {
+  operator fun component10(): J = value[9] as J
+}
+
+inline class tupleOf private constructor(override val value: Array<Any?>) :
+  Tuple10<Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing> {
   companion object {
     operator fun <A, B> invoke(a: A, b: B): Tuple2<A, B> =
       tupleOf(arrayOf(a, b))
@@ -61,5 +87,23 @@ inline class tupleOf private constructor(override val value: Array<Any?>) : Tupl
 
     operator fun <A, B, C, D> invoke(a: A, b: B, c: C, d: D): Tuple4<A, B, C, D> =
       tupleOf(arrayOf(a, b, c, d))
+
+    operator fun <A, B, C, D, E> invoke(a: A, b: B, c: C, d: D, e: E): Tuple5<A, B, C, D, E> =
+      tupleOf(arrayOf(a, b, c, d, e))
+
+    operator fun <A, B, C, D, E, F> invoke(a: A, b: B, c: C, d: D, e: E, f: F): Tuple6<A, B, C, D, E, F> =
+      tupleOf(arrayOf(a, b, c, d, e, f))
+
+    operator fun <A, B, C, D, E, F, G> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G): Tuple7<A, B, C, D, E, F, G> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g))
+
+    operator fun <A, B, C, D, E, F, G, H> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H): Tuple8<A, B, C, D, E, F, G, H> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h))
+
+    operator fun <A, B, C, D, E, F, G, H, I> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I): Tuple9<A, B, C, D, E, F, G, H, I> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J): Tuple10<A, B, C, D, E, F, G, H, I, J> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j))
   }
 }
