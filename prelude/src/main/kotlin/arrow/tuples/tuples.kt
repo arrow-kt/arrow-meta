f8aa415ede06288c761444cd242fc40da9f48e77
danimontoya <daniel.montoya@47deg.com>
2020-07-07 11:51:10 +0200

    Add tuples operators up until 22

--- prelude/src/main/kotlin/arrow/tuples/tuples.kt
+++ prelude/src/main/kotlin/arrow/tuples/tuples.kt
@@ -76,8 +76,56 @@ interface Tuple10<out A, out B, out C, out D, out E, out F, out G, out H, out I,
   operator fun component10(): J = value[9] as J
 }
 
+interface Tuple11<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K> : Tuple10<A, B, C, D, E, F, G, H, I, J> {
+  operator fun component11(): K = value[10] as K
+}
+
+interface Tuple12<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K, out L> : Tuple11<A, B, C, D, E, F, G, H, I, J, K> {
+  operator fun component12(): L = value[11] as L
+}
+
+interface Tuple13<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K, out L, out M> : Tuple12<A, B, C, D, E, F, G, H, I, J, K, L> {
+  operator fun component13(): M = value[12] as M
+}
+
+interface Tuple14<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K, out L, out M, out N> : Tuple13<A, B, C, D, E, F, G, H, I, J, K, L, M> {
+  operator fun component14(): N = value[13] as N
+}
+
+interface Tuple15<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K, out L, out M, out N, out O> : Tuple14<A, B, C, D, E, F, G, H, I, J, K, L, M, N> {
+  operator fun component15(): O = value[14] as O
+}
+
+interface Tuple16<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K, out L, out M, out N, out O, out P> : Tuple15<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O> {
+  operator fun component16(): P = value[15] as P
+}
+
+interface Tuple17<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K, out L, out M, out N, out O, out P, out Q> : Tuple16<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P> {
+  operator fun component17(): Q = value[16] as Q
+}
+
+interface Tuple18<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K, out L, out M, out N, out O, out P, out Q, out R> : Tuple17<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q> {
+  operator fun component18(): R = value[17] as R
+}
+
+interface Tuple19<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K, out L, out M, out N, out O, out P, out Q, out R, out S> : Tuple18<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R> {
+  operator fun component19(): S = value[18] as S
+}
+
+interface Tuple20<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K, out L, out M, out N, out O, out P, out Q, out R, out S, out T> : Tuple19<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S> {
+  operator fun component20(): T = value[19] as T
+}
+
+interface Tuple21<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K, out L, out M, out N, out O, out P, out Q, out R, out S, out T, out U> : Tuple20<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T> {
+  operator fun component21(): U = value[20] as U
+}
+
+interface Tuple22<out A, out B, out C, out D, out E, out F, out G, out H, out I, out J, out K, out L, out M, out N, out O, out P, out Q, out R, out S, out T, out U, out V> : Tuple21<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U> {
+  operator fun component22(): V = value[21] as V
+}
+
 inline class tupleOf private constructor(override val value: Array<Any?>) :
-  Tuple10<Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing> {
+  Tuple22<Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing> {
   companion object {
     operator fun <A, B> invoke(a: A, b: B): Tuple2<A, B> =
       tupleOf(arrayOf(a, b))
@@ -105,5 +153,41 @@ inline class tupleOf private constructor(override val value: Array<Any?>) :
 
     operator fun <A, B, C, D, E, F, G, H, I, J> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J): Tuple10<A, B, C, D, E, F, G, H, I, J> =
       tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K): Tuple11<A, B, C, D, E, F, G, H, I, J, K> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K, L> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K, l: L): Tuple12<A, B, C, D, E, F, G, H, I, J, K, L> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k, l))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K, L, M> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K, l: L, m: M): Tuple13<A, B, C, D, E, F, G, H, I, J, K, L, M> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k, l, m))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K, L, M, N> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K, l: L, m: M, n: N): Tuple14<A, B, C, D, E, F, G, H, I, J, K, L, M, N> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k, l, m, n))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K, l: L, m: M, n: N, o: O): Tuple15<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K, l: L, m: M, n: N, o: O, p: P): Tuple16<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K, l: L, m: M, n: N, o: O, p: P, q: Q): Tuple17<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K, l: L, m: M, n: N, o: O, p: P, q: Q, r: R): Tuple18<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K, l: L, m: M, n: N, o: O, p: P, q: Q, r: R, s: S): Tuple19<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K, l: L, m: M, n: N, o: O, p: P, q: Q, r: R, s: S, t: T): Tuple20<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K, l: L, m: M, n: N, o: O, p: P, q: Q, r: R, s: S, t: T, u: U): Tuple21<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u))
+
+    operator fun <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V> invoke(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K, l: L, m: M, n: N, o: O, p: P, q: Q, r: R, s: S, t: T, u: U, v: V): Tuple22<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V> =
+      tupleOf(arrayOf(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v))
   }
 }
