ac8c0b5201ec71d72db51089ea746db230f5a1fc
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-08-24 18:36:03 +0200

    Simplify code

--- meta-test/src/main/kotlin/arrow/meta/plugin/testing/Compilation.kt
+++ meta-test/src/main/kotlin/arrow/meta/plugin/testing/Compilation.kt
@@ -6,21 +6,26 @@ import com.tschuchort.compiletesting.SourceFile
 import io.github.classgraph.ClassGraph
 import org.assertj.core.api.Assertions.assertThat
 import java.io.File
+import java.io.PrintStream
 
 internal const val DEFAULT_FILENAME = "Source.kt"
 
-internal fun compile(data: CompilationData): Result {
-  val outputFile = File.createTempFile("tmp", "log")
-  val compilationResult = KotlinCompilation().apply {
+internal fun compile(data: CompilationData): Result =
+  KotlinCompilation().apply {
     sources = data.sources.map { SourceFile.kotlin(it.filename, it.text.trimMargin()) }
     classpaths = data.dependencies.map { classpathOf(it) }
     pluginClasspaths = data.compilerPlugins.map { classpathOf(it) }
     compilerPlugins = data.metaPlugins
-    messageOutputStream = outputFile.outputStream()
+    messageOutputStream = object : PrintStream(System.out) {
+      override fun write(buf: ByteArray, off: Int, len: Int) {
+        val newLine = String(buf, off, len)
+          .run { replace(Regex("^e:"), "error found:") }
+          .toByteArray()
+
+        super.write(newLine, off, newLine.size)
+      }
+    }
   }.compile()
-  rewriteAndPrintOutput(outputFile.readLines())
-  return compilationResult
-}
 
 private fun rewriteAndPrintOutput(lines: List<String>) {
   println(lines.joinToString(System.lineSeparator()) { it.replace(Regex("^e:"), "error found:") })
