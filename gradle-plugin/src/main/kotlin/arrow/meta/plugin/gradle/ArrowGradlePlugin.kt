a7c3c23817ceb337752ef1a7e1449ec600483c78
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-09-07 15:33:05 +0200

    Replace maven plugin and improve dependencies

--- gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/ArrowGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/ArrowGradlePlugin.kt
@@ -5,6 +5,7 @@ import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.io.File
 import io.github.classgraph.ClassGraph
+import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
 import java.util.Properties
 
 /**
@@ -26,9 +27,13 @@ class ArrowGradlePlugin : Plugin<Project> {
     val properties = Properties()
     properties.load(this.javaClass.getResourceAsStream("plugin.properties"))
     val compilerPluginVersion = properties.getProperty("COMPILER_PLUGIN_VERSION")
+    val kotlinVersion = project.getKotlinPluginVersion()
     project.extensions.create("arrow", ArrowExtension::class.java)
     project.afterEvaluate { p ->
-      p.dependencies.add("kotlinCompilerClasspath", "io.arrow-kt:compiler-plugin:$compilerPluginVersion")
+      // Dependencies that aren't provided by compiler-plugin
+      p.dependencies.add("kotlinCompilerClasspath", "org.jetbrains.kotlin:kotlin-script-util:$kotlinVersion")
+      p.dependencies.add("kotlinCompilerClasspath", "org.jetbrains.kotlin:kotlin-compiler-embeddable:$kotlinVersion")
+      p.dependencies.add("kotlinCompilerClasspath", "org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:$kotlinVersion")
 
       p.tasks.withType(KotlinCompile::class.java).configureEach {
         it.kotlinOptions.freeCompilerArgs += "-Xplugin=${classpathOf("compiler-plugin:$compilerPluginVersion")}"
@@ -39,7 +44,6 @@ class ArrowGradlePlugin : Plugin<Project> {
       task.group = "Arrow Meta"
       task.description = "Installs the correspondent Arrow Meta IDE Plugin if it's not already installed."
     }
-
     when {
       inIdea() && pluginsDirExists() && !ideaPluginExists() -> {
         println("Arrow Meta IDE Plugin is not installed!")
