be54cf19848b107a797063cbd111d8d7334fbd20
Imran Settuba <46971368+i-walker@users.noreply.github.com>
2020-10-05 19:13:24 +0200

    Update compiler-plugin/src/test/kotlin/arrow/meta/ir/syntax/Utils.kt
    Co-authored-by: Rachel M. Carmena <rachelcarmena@users.noreply.github.com>

--- compiler-plugin/src/test/kotlin/arrow/meta/ir/syntax/Utils.kt
+++ compiler-plugin/src/test/kotlin/arrow/meta/ir/syntax/Utils.kt
@@ -1,9 +1,1 @@
-package arrow.meta.ir.syntax
-
-import arrow.meta.ir.plugin.IrSyntaxPlugin
-import arrow.meta.phases.codegen.ir.IrUtils
-import arrow.meta.plugin.testing.CompilerTest
-import arrow.meta.plugin.testing.assertThis
-import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
-import org.jetbrains.kotlin.ir.IrElement
 
