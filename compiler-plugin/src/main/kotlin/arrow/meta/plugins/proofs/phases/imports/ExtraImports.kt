ef8614b86b2c4d5fc23493e2b3737cd4c58e5401
i-walker <46971368+i-walker@users.noreply.github.com>
2020-10-16 18:31:30 +0200

    add withAlias

--- compiler-plugin/src/main/kotlin/arrow/meta/plugins/proofs/phases/imports/ExtraImports.kt
+++ compiler-plugin/src/main/kotlin/arrow/meta/plugins/proofs/phases/imports/ExtraImports.kt
@@ -5,10 +5,11 @@ import arrow.meta.phases.ExtensionPhase
 import arrow.meta.plugins.proofs.phases.ArrowProofSet
 import arrow.meta.plugins.proofs.phases.ArrowRefined
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
 
 val Meta.preludeImports: ExtensionPhase
   get() = extraImports { _ ->
     ArrowProofSet.map { importInfo(it) } +
-      (1..22).map { importInfo(FqName("arrow.with.n$it")) } +
+      (1..22).map { importInfo(FqName("arrow.with.n$it"), alias = Name.identifier("with$it")) } +
       importInfo(FqName("arrow.given")) + importInfo(ArrowRefined)
   }
