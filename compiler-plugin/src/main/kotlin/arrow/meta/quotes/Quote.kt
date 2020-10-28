2e9a8a83e01e29f5416582bd811e6dd5d3cd8989
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-10-28 12:51:33 +0100

    consider nullable baseDir

--- compiler-plugin/src/main/kotlin/arrow/meta/quotes/Quote.kt
+++ compiler-plugin/src/main/kotlin/arrow/meta/quotes/Quote.kt
@@ -389,7 +389,7 @@ fun CompilerContext.changeSource(file: KtFile, newSource: String, rootFile: KtFi
   var virtualFile = rootFile.virtualFile
   if (sourcePath != null) {
     val baseDir = configuration?.get(ArrowMetaConfigurationKeys.GENERATED_SRC_OUTPUT_DIR, listOf(DEFAULT_BASE_DIR.toString()))?.get(0)
-    val path = Paths.get(baseDir, sourcePath)
+    val path = Paths.get(baseDir ?: DEFAULT_BASE_DIR.toString(), sourcePath)
     val directory = path.toFile()
     directory.mkdirs()
     virtualFile = CoreLocalVirtualFile(CoreLocalFileSystem(), File(directory, file.name).apply {
