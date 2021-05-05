2d6192620494208543953d41888e78b30a485da4
raulraja <raulraja@gmail.com>
2021-05-05 18:08:23 +0200

    remove test refinement

--- prelude/src/main/kotlin/arrow/refinement/numbers.kt
+++ prelude/src/main/kotlin/arrow/refinement/numbers.kt
@@ -14,10 +14,3 @@ value class Even private constructor(val value: Int) {
     ensure((it % 2 == 0) to "$it should be even")
   })
 }
-
-@JvmInline
-value class ContentType private constructor(val value: String) {
-  companion object : Refined<String, ContentType>(::ContentType, {
-    ensure((it.equals("content-type", ignoreCase = true)) to "$it should be named Content-Type")
-  })
-}
