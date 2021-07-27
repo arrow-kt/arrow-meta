48e005e7a2717b5fc8675c34635dd4a462e7052c
Alejandro Serrano <trupill@gmail.com>
2021-07-27 15:37:17 +0200

    Implement poor continuations

---
+++ refined-types-plugin/src/main/kotlin/arrow/meta/PoorContinuations.kt
@@ -0,0 +1,41 @@
+package arrow.meta
+
+typealias Cont<R, A> = (cont: (x: A) -> R) -> R
+
+fun <A> Cont<A, A>.runCont(): A = this { it }
+
+fun <R, A> continueWith(x: A): Cont<R, A> = { cont -> cont(x) }
+fun <R, A> abortWith(r: R): Cont<R, A> = { _ -> r }
+
+fun <R, A, B> Cont<R, A>.then(f: ((a: A) -> Cont<R, B>)): Cont<R, B> =
+  { cont: (r: B) -> R -> this { a -> f(a)(cont) } }
+fun <R, A, B> Cont<R, A>.par(cont: () -> Cont<R, B>): Cont<R, Pair<A, B>> =
+  this.then { a -> cont().then { b -> continueWith(Pair(a, b)) } }
+
+fun <R, A> Cont<R, A>.forget(): Cont<R, Unit> =
+  this.then { continueWith(Unit) }
+
+fun <R, A, B> List<A>.contEach(f: ((a: A) -> Cont<R, B>)): Cont<R, List<B>> =
+  if (this.isEmpty())
+    continueWith<R, List<B>>(emptyList())
+  else
+    f(this.first()).par {
+      this.drop(1).contEach(f)
+    }.then { (b, bs) ->
+      continueWith(listOf(b) + bs)
+    }
+fun <R, A> Int.contEach(f: (n: Int) -> Cont<R, A>): Cont<R, List<A>> =
+  List(this, { it }).contEach { n -> f(n) }
+
+fun main(args: Array<String>): Unit {
+  3.contEach<Unit, Unit> {
+    println("Hello $it")
+    continueWith(Unit)
+  }.forget().runCont()
+
+  3.contEach<Unit, Unit> { n ->
+    println("Bye $n")
+    if (n >= 1) abortWith(Unit)
+    else continueWith(Unit)
+  }.forget().runCont()
+}
