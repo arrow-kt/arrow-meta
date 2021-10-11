9bc70ae7718fab6e30714093573dfe1cbbf0f0b6
Alejandro Serrano <trupill@gmail.com>
2021-10-11 15:20:39 +0200

    Missing implementations

--- plugins/analysis/common/src/main/kotlin/arrow/meta/plugins/analysis/phases/analysis/solver/ast/context/elements/InitializerList.kt
+++ plugins/analysis/common/src/main/kotlin/arrow/meta/plugins/analysis/phases/analysis/solver/ast/context/elements/InitializerList.kt
@@ -1,5 +1,5 @@
 package arrow.meta.plugins.analysis.phases.analysis.solver.ast.context.elements
 
-interface SuperTypeList : Element {
+interface InitializerList : Element {
   val entries: List<SuperTypeListEntry>
 }
