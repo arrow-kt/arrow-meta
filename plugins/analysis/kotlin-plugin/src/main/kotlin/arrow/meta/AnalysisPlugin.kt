9f613e479199f69b924790507880822075d97a77
Alejandro Serrano <trupill@gmail.com>
2021-10-20 10:38:14 +0200

    Kotlin 1.6 is pickier

--- plugins/analysis/kotlin-plugin/src/main/kotlin/arrow/meta/AnalysisPlugin.kt
+++ plugins/analysis/kotlin-plugin/src/main/kotlin/arrow/meta/AnalysisPlugin.kt
@@ -2,10 +2,8 @@ package arrow.meta
 
 import arrow.meta.phases.CompilerContext
 import arrow.meta.plugins.analysis.phases.analysisPhases
-import kotlin.contracts.ExperimentalContracts
 
 open class AnalysisPlugin : Meta {
-  @ExperimentalContracts
   override fun intercept(ctx: CompilerContext): List<CliPlugin> =
     listOf(
       "Arrow Analysis" {
