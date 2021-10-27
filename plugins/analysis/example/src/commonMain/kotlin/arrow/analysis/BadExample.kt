cc44a5ab640c6d4a70b9f1d891b71d86e2891454
Alejandro Serrano <trupill@gmail.com>
2021-10-27 13:28:08 +0200

    Quick Start

--- plugins/analysis/example/src/commonMain/kotlin/arrow/analysis/BadExample.kt
+++ plugins/analysis/example/src/commonMain/kotlin/arrow/analysis/BadExample.kt
@@ -7,3 +7,16 @@ fun bar(x: Int): Int {
   return 1 / x
 }
 // val other: Int = emptyList<Int>().get(1)
+
+fun increment(x: Int): Int {
+  pre(x > 0) { "value must be positive" }
+  return (x + 1).post({ it > 0 }) { "result is positive" }
+}
+val example = increment(increment(1))
+
+class Positive(val value: Int) {
+  init { require(value > 0) }
+}
+val positiveExample = Positive(1)
+fun Positive.add(other: Positive) =
+  Positive(this.value + other.value)
