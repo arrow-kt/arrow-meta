7fcf2f70c173d493bf50a120bc5fc26849fbf0de
Alejandro Serrano <trupill@gmail.com>
2021-10-29 15:28:17 +0200

    [Analysis] Documentation (#891)
    * Layout for Analysis docs
    
    * Quick Start
    
    * Fix a problem with conditionals
    
    * Pre- and post-conditions and control operators
    
    * More docs
    
    * Mutability and loops
    
    * JS is fijn
    
    * CSS is fijn
    
    * Types and invariants
    
    * Fields
    
    * 3rd party libraries
    
    * Rework a bit the sidebar
    
    * Add imports
    
    Co-authored-by: Raúl Raja Martínez <raulraja@gmail.com>

--- plugins/analysis/example/src/commonMain/kotlin/arrow/analysis/BadExample.kt
+++ plugins/analysis/example/src/commonMain/kotlin/arrow/analysis/BadExample.kt
@@ -7,3 +7,59 @@ fun bar(x: Int): Int {
   return 1 / x
 }
 // val other: Int = emptyList<Int>().get(1)
+
+fun increment(x: Int): Int {
+  pre(x > 0) { "value must be positive" }
+  return (x + 1).post({ it > 0 }) { "result is positive" }
+}
+val example = increment(increment(1))
+
+class Positive(val value: Int) {
+  init { require(value > 0) }
+}
+val positiveExample = Positive(1)
+fun Positive.add(other: Positive) =
+  Positive(this.value + other.value)
+
+/*
+fun nope(x: Int): Int {
+  pre(x >= 0) { "value >= 0" }
+  return (x + 1).post({ it > 1 }) { "result > 1" }
+}
+
+fun nope2(x: Int): Int {
+  pre(x > 0) { "x must be positive" }
+  return if (x < 0) 1 else 2
+}
+*/
+
+fun <A> List<A>.myGet(index: Int): A {
+  pre(index >= 0 && index < this.size) { "index within bounds" }
+  return this.get(index)
+}
+fun <A> List<A>.firstOr(default: A): A =
+  if (this.size > 0) this.myGet(0) else default
+
+fun absoluteValue(n: Int): Int = when {
+  n < 0 -> -n
+  n == 0 -> 0
+  else -> n
+}.post({ it >= 0 }) { "result >= 0" }
+
+fun double(n: Int): Int {
+  pre(n > 0) { "n positive" }
+  val z = n + n
+  val r = z + 1
+  return r.post({ it > 0 }) { "result positive" }
+}
+
+fun double2(n: Int): Int {
+  pre(n > 0) { "n positive" }
+  return (n + n).let { it + 1 }.post({ it > 0 }) { "result positive" }
+}
+
+fun <A> List<A>.count(): Int {
+  var count = 0.invariant({ it >= 0 }) { "z >= 0" }
+  for (elt in this) { count = count + 1 }
+  return count.post({ it >= 0 }) { "result >= 0" }
+}
