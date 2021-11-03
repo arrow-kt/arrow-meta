d0bce52a3d98d96e8d5d21f522a0513ab6e19d38
Alejandro Serrano <trupill@gmail.com>
2021-11-03 14:56:55 +0100

    Figured out how to create message bundles

--- plugins/analysis/java-plugin/src/main/kotlin/arrow/meta/plugins/analysis/java/AnalysisPlugin.kt
+++ plugins/analysis/java-plugin/src/main/kotlin/arrow/meta/plugins/analysis/java/AnalysisPlugin.kt
@@ -9,21 +9,40 @@ import com.sun.tools.javac.code.Lint
 import com.sun.tools.javac.util.Context
 import com.sun.tools.javac.util.DiagnosticSource
 import com.sun.tools.javac.util.JCDiagnostic
+import com.sun.tools.javac.util.JavacMessages
 import com.sun.tools.javac.util.Log
+import java.util.*
+
+@Suppress("unused")
+public class AnalysisMessages : ListResourceBundle() {
+  override fun getContents(): Array<Array<Any>> =
+    arrayOf(arrayOf("arrow-analysis.warn.hello", "Hello from {0}"))
+}
+
+public class AnalysisContext(context: Context) {
+  public val logger: Log = Log.instance(context)
+  private val messages: JavacMessages =
+    JavacMessages.instance(context).apply { add(AnalysisMessages::class.qualifiedName) }
+  public val diagnostics: JCDiagnostic.Factory = JCDiagnostic.Factory(messages, "arrow-analysis")
+
+  public companion object {
+    public operator fun invoke(task: BasicJavacTask): AnalysisContext =
+      AnalysisContext(task.context)
+  }
+}
 
 public class AnalysisPlugin : Plugin {
   override fun getName(): String = NAME
 
   override fun init(task: JavacTask?, vararg args: String?) {
-    val context: Context = (task as BasicJavacTask).context
-    val logger = Log.instance(context)
-    val diagnostics = JCDiagnostic.Factory.instance(context)
-    logger.report(
-      diagnostics.warning(
+    val ctx = AnalysisContext(task as BasicJavacTask)
+    ctx.logger.report(
+      ctx.diagnostics.warning(
         Lint.LintCategory.RAW,
         DiagnosticSource.NO_SOURCE,
         JCDiagnostic.SimpleDiagnosticPosition(1),
-        "Hello from $name"
+        "hello",
+        name
       )
     )
   }
