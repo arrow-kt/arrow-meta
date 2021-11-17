42668390afd58ce3158bf394407989dd68fe3209
Alejandro Serrano <trupill@gmail.com>
2021-11-17 10:41:04 +0100

    Use different compiler ids for each plugin (#920)

--- plugins/analysis/gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/AnalysisGradlePlugin.kt
+++ plugins/analysis/gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/AnalysisGradlePlugin.kt
@@ -4,8 +4,7 @@ public class AnalysisGradlePlugin : ArrowMetaGradlePlugin {
   override val groupId: String = "io.arrow-kt"
   override val artifactId: String = "arrow-analysis-kotlin-plugin"
   override val version: String = "1.5.31-SNAPSHOT"
-  override val pluginId: String = "io.arrow-kt.analysis"
-  override val extensionName: String = "arrowAnalysis"
+  override val pluginId: String = "analysis"
 
   override val dependencies: List<Triple<String, String, String>> =
     listOf(
--- plugins/optics/optics-gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/OpticsGradlePlugin.kt
+++ plugins/optics/optics-gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/OpticsGradlePlugin.kt
@@ -4,9 +4,8 @@ public class OpticsGradlePlugin : ArrowMetaGradlePlugin {
   override val groupId: String = "io.arrow-kt"
   override val artifactId: String = "arrow-optics-plugin"
   override val version: String = "1.5.31-SNAPSHOT"
-  override val pluginId: String = "io.arrow-kt.optics"
-  override val extensionName: String = "arrowOptics"
+  override val pluginId: String = "optics"
 
   override val dependencies: List<Triple<String, String, String>> =
-    listOf(Triple(groupId, "arrow-optics", "1.0.0"))
+    listOf(Triple(groupId, "arrow-optics", "1.0.1"))
 }
