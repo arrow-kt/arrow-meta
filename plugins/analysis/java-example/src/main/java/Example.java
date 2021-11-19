4ed779b0c522d435b0f26efd6b741b77487a00eb
Alejandro Serrano <trupill@gmail.com>
2021-11-19 17:03:02 +0100

    Introduce functional interfaces for Java compatibility

--- plugins/analysis/java-example/src/main/java/Example.java
+++ plugins/analysis/java-example/src/main/java/Example.java
@@ -3,7 +3,7 @@ import static arrow.analysis.RefinementDSLKt.pre;
 
 public class Example {
     public int f(int x) {
-        pre(x > 0, () -> "x mut be positive");
+        pre(x > 0, () -> "x must be positive");
         return post(x + 1, r -> r > 0, () -> "result is positive");
     }
 }
