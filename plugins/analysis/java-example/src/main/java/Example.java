413c4ba5807dc9b21dd67d6422fc51dbd78d3226
Alejandro Serrano <trupill@gmail.com>
2021-11-19 16:45:53 +0100

    Fix the example

--- plugins/analysis/java-example/src/main/java/Example.java
+++ plugins/analysis/java-example/src/main/java/Example.java
@@ -4,6 +4,6 @@ import static arrow.analysis.RefinementDSLKt.pre;
 public class Example {
     public int f(int x) {
         pre(x > 0, () -> "x mut be positive");
-        return post(x + 1, r -> r > 100, () -> "result is positive");
+        return post(x + 1, r -> r > 0, () -> "result is positive");
     }
 }
