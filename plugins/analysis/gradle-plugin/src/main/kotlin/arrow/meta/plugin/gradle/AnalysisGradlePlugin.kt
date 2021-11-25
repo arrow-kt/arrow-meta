c30625807e111b0f1f37130c4370f2f304c9ae59
Alejandro Serrano <trupill@gmail.com>
2021-11-25 10:52:06 +0100

    Update the dependencies added by plug-ins

--- plugins/analysis/gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/AnalysisGradlePlugin.kt
+++ plugins/analysis/gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/AnalysisGradlePlugin.kt
@@ -3,7 +3,7 @@ package arrow.meta.plugin.gradle
 public class AnalysisGradlePlugin : ArrowMetaGradlePlugin {
   override val groupId: String = "io.arrow-kt"
   override val artifactId: String = "arrow-analysis-kotlin-plugin"
-  override val version: String = "1.6.0-SNAPSHOT"
+  override val version: String = "1.0-SNAPSHOT"
   override val pluginId: String = "analysis"
 
   override val dependencies: List<Triple<String, String, String>> =
--- plugins/proofs/proofs-gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/ProofsGradlePlugin.kt
+++ plugins/proofs/proofs-gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/ProofsGradlePlugin.kt
@@ -3,7 +3,7 @@ package arrow.meta.plugin.gradle
 public class ProofsGradlePlugin : ArrowMetaGradlePlugin {
   override val groupId: String = "io.arrow-kt"
   override val artifactId: String = "arrow-proofs-plugin"
-  override val version: String = "1.6.0-SNAPSHOT"
+  override val version: String = "1.0-SNAPSHOT"
   override val pluginId: String = "proofs"
 
   override val dependencies: List<Triple<String, String, String>> =
