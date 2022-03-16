050b8463cca41d2eb916571e728a12bc076002bf
Alejandro Serrano <trupill@gmail.com>
2022-03-16 09:41:50 +0100

    Update 'java-smt' to 3.12

--- plugins/analysis/common/src/main/kotlin/arrow/meta/plugins/analysis/smt/FormulaExtensions.kt
+++ plugins/analysis/common/src/main/kotlin/arrow/meta/plugins/analysis/smt/FormulaExtensions.kt
@@ -6,29 +6,46 @@ import org.sosy_lab.java_smt.api.Formula
 import org.sosy_lab.java_smt.api.FormulaManager
 import org.sosy_lab.java_smt.api.FunctionDeclaration
 import org.sosy_lab.java_smt.api.visitors.DefaultFormulaVisitor
+import org.sosy_lab.java_smt.api.visitors.FormulaTransformationVisitor
 import org.sosy_lab.java_smt.api.visitors.TraversalProcess
+import org.sosy_lab.java_smt.basicimpl.FunctionDeclarationImpl
 
 fun <T : Formula> Solver.substituteVariable(formula: T, mapping: Map<String, Formula>): T =
+  try {
     formulae {
-  val subst = mapping.mapKeys { entry -> makeVariable(getFormulaType(entry.value), entry.key) }
-  substitute(formula, subst)
-}
+      val subst = mapping.mapKeys { entry -> makeVariable(getFormulaType(entry.value), entry.key) }
+      substituteWithFix(formula, subst)
+    }
+  } catch (e: Exception) {
+    throw java.lang.IllegalArgumentException("substituting $formula with $mapping", e)
+  }
 
 internal fun <T : Formula> Solver.renameObjectVariables(
   formula: T,
   mapping: Map<String, String>
-): T = formulae {
-  val subst = mapping.map { (k, v) -> Pair(makeObjectVariable(k), makeObjectVariable(v)) }.toMap()
-  substitute(formula, subst)
-}
+): T =
+  try {
+    formulae {
+      val subst =
+        mapping.map { (k, v) -> Pair(makeObjectVariable(k), makeObjectVariable(v)) }.toMap()
+      substituteWithFix(formula, subst)
+    }
+  } catch (e: Exception) {
+    throw java.lang.IllegalArgumentException("renaming $formula with $mapping", e)
+  }
 
 internal fun <T : Formula> Solver.substituteObjectVariables(
   formula: T,
   mapping: Map<String, ObjectFormula>
-): T = formulae {
-  val subst = mapping.map { (k, v) -> Pair(makeObjectVariable(k), v) }.toMap()
-  substitute(formula, subst)
-}
+): T =
+  try {
+    formulae {
+      val subst = mapping.map { (k, v) -> Pair(makeObjectVariable(k), v) }.toMap()
+      substituteWithFix(formula, subst)
+    }
+  } catch (e: Exception) {
+    throw java.lang.IllegalArgumentException("substituting $formula with $mapping", e)
+  }
 
 fun Solver.renameDeclarationConstraints(
   decl: DeclarationConstraints,
@@ -106,3 +123,39 @@ fun Solver.isFieldCall(f: Formula): Boolean {
 
 fun FormulaManager.extractSingleVariable(formula: Formula): String? =
   extractVariables(formula).takeIf { it.size == 1 }?.toList()?.getOrNull(0)?.first
+
+// our version of substitute, needed to work around
+// a bug with SMTInterpol
+fun <T : Formula> Solver.substituteWithFix(pF: T, pFromToMapping: Map<out Formula, Formula>): T =
+  transformRecursively(
+    pF,
+    object : FormulaTransformationVisitor(this) {
+      override fun visitFreeVariable(f: Formula, name: String): Formula = replace(f)
+
+      override fun visitFunction(
+        f: Formula,
+        newArgs: List<Formula>,
+        functionDeclaration: FunctionDeclaration<*>
+      ): Formula = pFromToMapping[f] ?: makeApplicationSafe(functionDeclaration, newArgs)
+
+      private fun replace(f: Formula): Formula = pFromToMapping[f] ?: f
+
+      private fun makeApplicationSafe(fn: FunctionDeclaration<*>, args: List<Formula>) =
+        try {
+          makeApplication(fn, args)
+        } catch (e: IndexOutOfBoundsException) {
+          fn as FunctionDeclarationImpl<*, *>
+          val newFn =
+            FunctionDeclarationImpl.of(
+              fn.name,
+              fn.kind,
+              fn.argumentTypes.take(2),
+              fn.type,
+              fn.solverDeclaration
+            )
+          // this happens sometimes in SMTInterpol,
+          // so we try to fix it by using the function with each 2 arguments
+          args.reduce { acc, f -> makeApplication(newFn, listOf(acc, f)) }
+        }
+    }
+  )
